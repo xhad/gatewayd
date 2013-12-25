35dfff69c419c3a5bdc950f7fd08ab696027f957
Steven Zeiler <zeiler.steven@gmail.com>
2013-12-25 00:47:15 +0000

    [CHORE] Remove broken belongsTo.

--- models/bank_tx.js
+++ models/bank_tx.js
@@ -6,11 +6,12 @@ var BankTx = sequelize.define('bank_transaction', {
   id: { type: Sequelize.INTEGER, unique: true, primaryKey: true, autoIncrement: true },
   deposit: Sequelize.BOOLEAN,
   currency: { type: Sequelize.STRING, allowNull: false },
-  bankAccountId: { type: Sequelize.INTEGER, allowNull: false },
+  bankAccountId: { 
+		type: Sequelize.INTEGER,
+		allowNull: false
+	},
   cashAmount: { type: Sequelize.FLOAT(11,12), allowNull: false },
   rippleTxId: Sequelize.INTEGER
 });
-
-BankTx.belongsTo(BankAccount, {foreignKeyConstraint: true});
 
 module.exports = BankTx;
