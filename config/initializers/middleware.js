a1916d154ede1b7ad1ea952a85f230ed312c8bfd
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-06 12:43:28 -0800

    [FEATURE] Allow all headers in middleware

--- config/initializers/middleware.js
+++ config/initializers/middleware.js
@@ -8,7 +8,7 @@ module.exports = (function(){
     app.use(express.static(__dirname + '/public'));
     app.use(function(req,res,next) {
       res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      res.header("Access-Control-Allow-Headers", "*");
       next()
     })
     app.use(express.bodyParser())
