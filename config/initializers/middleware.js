8b4977b2138c3323419740f93ec4d5d0f5f7d1bf
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-06 12:47:34 -0800

    [FEATURE] Allow several headers in middleware

--- config/initializers/middleware.js
+++ config/initializers/middleware.js
@@ -5,19 +5,19 @@ module.exports = (function(){
   function configure(app) {
     app.set('port', process.env.PORT || 3000);
     app.set('host', process.env.HOST || '127.0.0.1')
-    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/public'))
     app.use(function(req,res,next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "*");
+      res.header("Access-Control-Allow-Origin", "*")
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
       next()
     })
     app.use(express.bodyParser())
     app.use(expressValidator());
-    app.use(express.cookieParser());
-    app.use(express.session({secret: 'oi09ajsdf09fwlkej33lkjpx'}));
+    app.use(express.cookieParser())
+    app.use(express.session({secret: 'oi09ajsdf09fwlkej33lkjpx'}))
     app.use(express.methodOverride())
     app.use(function(err, req, res, next) {
-      res.send({ error: err });
+      res.send({ error: err })
     });
   }
   return { configure: configure }
