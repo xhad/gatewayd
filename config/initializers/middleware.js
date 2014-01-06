0a6cd180684f36a5b3f78e720206f28650d0018c
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-06 12:41:31 -0800

    [FEATURE] Properly use Access-Control-Allow-Origin middlewar

--- config/initializers/middleware.js
+++ config/initializers/middleware.js
@@ -9,6 +9,7 @@ module.exports = (function(){
     app.use(function(req,res,next) {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      next()
     })
     app.use(express.bodyParser())
     app.use(expressValidator());
