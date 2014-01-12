ca0d3f81371a8bec0122182e568796761e40367b
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-12 02:25:09 -0800

    [FEATURE] Refactor config/routes to match rails routes

--- config/routes.js
+++ config/routes.js
@@ -1,46 +1,44 @@
 module.exports = (function(){
   function configure(app, ctrls) {
-    app.get('/', function(req, res){
-      res.render('index.html') 
-    })
-    // UsersController
-    app.get('/v1/gateway/users/:userId/gateway_account', ctrls['users'].account)
-    app.post('/v1/gateway/users', ctrls['users'].create)
+    app.get('/', function(req, res){ res.render('index.html') })
 
-    // GatewayAccountsController
-    app.post('/v1/gateway/users/:userId/gateway_accounts', ctrls['users'].account)
-    app.get('/v1/gateway/accounts/:accountId/balances', 
-        ctrls['balances'].gatewayAccountBalances)
-    app.post('/v1/gateway/accounts/:accountId/deposits', ctrls['deposits'].create);
-    app.get('/v1/gateway/accounts/:accountId/transactions', 
-        ctrls['gateway_transactions'].forAccount)
+    get('/v1/gateway/users/:userId/gateway_account', 'users#account')
+    post('v1/gateway/users', 'users#create')
+    post('/v1/gateway/users/:userId/gateway_accounts', 'users#account')
+    get('/v1/gateway/accounts/:accountId/balances', 'balances#gatewayAccountBalances')
+    post('/v1/gateway/accounts/:accountId/deposits', 'deposits#create')
+    get('/v1/gateway/accounts/:accountId/transactions', 'gateway_transactions#forAccount')
+    get('/v1/users/:userId/gateway/accounts', 'accounts#userAccount')
+    get('/v1/users/:userId/ripple_addresses','ripple_addresses#userIndex')
+    post('/v1/users/:userId/gateway/accounts', 'accounts#create')
+    post('/v1/users/:userId/ripple_addresses', 'ripple_addresses#create')
 
-    app.get('/v1/users/:userId/gateway/accounts',
-        ctrls['accounts'].userIndex)
-    app.get('/v1/users/:userId/ripple_addresses',
-        ctrls['ripple_addresses'].userIndex)
+    post('/v1/gateway/accounts/:accountId/withdrawals', 'withdrawals#create')
+    get('/v1/gateway_transactions', 'gateway_transactions#index')
+    get('/v1/withdrawals', 'withdrawals#index')
+    get('/v1/deposits', 'deposits#index')
 
-    app.post('/v1/users/:userId/gateway/accounts', 
-        ctrls['accounts'].create);
-    app.post('/v1/users/:userId/ripple_addresses', 
-        ctrls['ripple_addresses'].create);
-
-    app.post('/v1/gateway/accounts/:accountId/withdrawals', ctrls['withdrawals'].create);
-
-    app.get('/v1/gateway_transactions', ctrls['gateway_transactions'].index);
-    app.get('/v1/withdrawals', ctrls['withdrawals'].index);
-    app.get('/v1/deposits', ctrls['deposits'].index);
-
-    app.get('/v1/gateway/accounts', ctrls['accounts'].index);
-    app.post('/v1/gateway/accounts', ctrls['accounts'].create);
-    app.get('/v1/balances', ctrls['balances'].index);
-    app.get('/v1/ripple_addresses', ctrls['ripple_addresses'].index);
-    app.get('/v1/ripple_transactions', ctrls['ripple_transactions'].index);
-    app.post('/v1/ripple_transactions', ctrls['ripple_transactions'].create);
-    app.get('/v1/ripple_transactions/:id', ctrls['ripple_transactions'].show);
-    app.put('/v1/ripple_transactions/:id', ctrls['ripple_transactions'].update);
+    get('/v1/gateway/accounts', 'accounts#index')
+    post('/v1/gateway/accounts', 'accounts#create')
+    get('/v1/balances', 'balances#index')
+    get('/v1/ripple_addresses', 'ripple_addresses#index')
+    get('/v1/ripple_transactions', 'ripple_transactions#index')
+    post('/v1/ripple_transactions', 'ripple_transactions#create')
+    get('/v1/ripple_transactions/:id', 'ripple_transactions#show')
+    put('/v1/ripple_transactions/:id', 'ripple_transactions#update')
   }
+
+  function route(method, path, controllerAction) {
+    controllerAction = controllerAction.split('#')[0]
+    controller = controllerAction[0]
+    action = controllerAction[1]
+    app[method](path, controller][action])
+  }
+
+  function get(path, action) { route('get', path, action) } 
+  function put(path, action) { route('put', path, action) }
+  function post(path, action) { route('post', path, action) }
+  function del(path, action) { route('delete', path, action) }
 
   return { configure: configure  }
 })()
-
