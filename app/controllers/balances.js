c964725667114d1474ea8518c94dfa9fadbec2fd
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-12 02:26:53 -0800

    [CHORE] Remove all functionality from the balances controller

--- app/controllers/balances.js
+++ app/controllers/balances.js
@@ -2,35 +2,4 @@ var Balance = require('../models/balance.js');
 
 module.exports = (function(){
 
-  function gatewayAccountBalances(req, res) {
-		req.checkParams('accountId', 'Invalid name')
-			.notEmpty()
-      .isNumeric();
-		
-		var errors = req.validationErrors();
-		if (errors) {
-			res.send({ success: false, error: util.inspect(errors) }, 400)
-			return;
-    }
-    
-    Balance.findAll({ where: { bankAccountId: req.params.accountId }})
-    .complete(function(err, balances){
-      if (err) { 
-        res.send({ success: false, error: err })
-      }
-		  res.send({ success: true, balances: balances })
-    })
-	} 
-
-  function index(req, res){
-		Balance.findAll().complete(function(err, balances){
-      if (err) { res.send({ success: false, error: err }) }
-			res.send({ success: true, balances: balances });
-    })
-	}
-
-	return {
-		gatewayAccountBalances: gatewayAccountBalances,
-		index: index
-	}
 })();
