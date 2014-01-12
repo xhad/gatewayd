8ab0bd0ecf0fcbf28d798217c7831b588edda1e0
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-12 02:47:51 -0800

    [FEATURE] Add userAccount action to gateway_accounts, properly format response json with success status.

--- app/controllers/accounts.js
+++ app/controllers/accounts.js
@@ -2,11 +2,11 @@ var GatewayAccount = require('../models/gateway_account')
 var util = require('util')
 
 module.exports = (function(){
-	function userIndex(req, res) {
-		GatewayAccount.findAll({ where: { userId: req.params.userId }})
-    .complete(function(err, accounts) {
-      if (err) {res.send({ error: err }); return }
-			res.send(accounts);
+	function userAccount(req, res) {
+		GatewayAccount.find({ where: { userId: req.params.userId }})
+    .complete(function(err, account) {
+      if (err) {res.send({ success: false, error: err }); return }
+			res.send({ success: true, account: account })
     })
   }
   
@@ -14,7 +14,7 @@ module.exports = (function(){
 		GatewayAccount.create({
 			userId: req.params.userId,	
 		}).complete(function(err, account){
-      if (err) {res.send({ error: err }); return }
+      if (err) {res.send({ success: false, error: err }); return }
 			res.send({ success: true, gatewayAccount: account })
     })
 	}
@@ -22,13 +22,13 @@ module.exports = (function(){
   function index(req, res) {
     GatewayAccount.findAll()
     .complete(function(err, accounts) {
-      if (err) {res.send({ error: err }); return }
+      if (err) {res.send({ success: false, error: err }); return }
 			res.send(accounts)
     })
 	}
 
 	return {
-		userIndex: userIndex,
+    userAccount: userAccount,
 		create: create,
 		index: index
 	}
