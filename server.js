07acaed9cdd8ed4073aa4ce8782170c826ecda32
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-13 23:12:01 -0800

    [FEATURE] Add route for creating transactions

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.post('/api/v1/gateway/users', ctrls['gateway_users'].create)
 app.get('/api/v1/sessions', ctrls['sessions'].show)
 app.post('/api/v1/sessions/destroy', ctrls['sessions'].destroy)
 
+app.post('/api/v1/ripple_transactions', ctrls['ripple_transactions'].create)
+
 address = process.env.ADDRESS
 port = process.env.PORT || 4000
 
