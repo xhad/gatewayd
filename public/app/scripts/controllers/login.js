a188e93170c755c64fff713f8bfe00e823001c40
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-14 03:52:56 -0800

    [FEATURE] Update the session always in the login controller

--- public/app/scripts/controllers/login.js
+++ public/app/scripts/controllers/login.js
@@ -4,12 +4,18 @@ angular.module('publicApp')
   .controller('LoginCtrl', ['$scope', '$http', '$location', 'UserService', function ($scope, $http, $location, $user) {
     $scope.user = {}
 
+    $user.updateSession(function(user){
+      if (user.isLogged){
+        console.log('user is logged in from the session')
+        $user.
+        $location.path('/gatway_account');
+      } 
+    })
+
     $scope.login = function () {
 			var name = $scope.user.name;
 			var password = $scope.user.password;
       $user.login(name, password, function(err, session){
-        console.log(err)
-        console.log(session)
         if (!!session.username) {
 					$location.path('/gatway_account');
 				} else {
