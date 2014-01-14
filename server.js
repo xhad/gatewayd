5cba3a661c91787da38f40dda532cf6a13e9d3df
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-14 12:32:03 -0800

    [FEATURE] Add server authentication library

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var requireAll = require('./lib/require-all')
+var authorization = require('express-authorization')
 
 ctrls = requireAll({
   dirname: __dirname + '/app/controllers',
@@ -12,12 +13,20 @@ app.use(express.static(__dirname + '/public'))
 require('./config/initializers/middleware.js').configure(app)
 //require('./config/routes').configure(app, ctrls)
 
+app.get('/authenticated/resource', authorization.ensureRequest.isPermitted("user:view"), function(req, res){
+  res.send({ success: 'access granted' })
+})
+
+app.get('/auth', function(req, res){
+  req.session.user = { permissions: ['user:basic'] }
+  res.send({ success: true })
+})
 
 app.post('/api/v1/users', ctrls['users'].create)
 app.post('/api/v1/sessions', ctrls['sessions'].create)
 app.post('/api/v1/gateway/users', ctrls['gateway_users'].create)
 app.get('/api/v1/sessions', ctrls['sessions'].show)
-app.post('/api/v1/sessions/destroy', ctrls['sessions'].destroy)
+app.get('/api/v1/sessions/destroy', ctrls['sessions'].destroy)
 
 app.post('/api/v1/ripple_transactions', ctrls['ripple_transactions'].create)
 app.get('/api/v1/ripple_transactions', ctrls['ripple_transactions'].index)
