ca6cc7b198c387e354484e579ae9d99ab85f2a07
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-14 03:30:36 -0800

    [FEATURE] Add login function to user service

--- public/app/scripts/services/user.js
+++ public/app/scripts/services/user.js
@@ -1,7 +1,26 @@
-angular.module('publicApp').service('UserService', ['$http', function($http) {
+angular.module('publicApp').service('UserService', ['$http','$location', function($http, $location) {
   var user = {
     isLogged: false,
-    username: ''
+    username: '',
+
+    login: function(username, password) {
+      user = this
+			$http.post('/api/v1/sessions', {
+				name: username,
+				password: password
+			}).success(function(response){
+        if (!!response.session.username) {
+          this.isLogged = true,
+          this.username = username
+          console.log('about to redirect to /gateway_account')
+					$location.path('/gatway_account');
+				} else {
+					$location.path('/admin/users/new');
+				}
+			}).error(function(){
+    
+      })
+    }
   } 
   return user
 }])
