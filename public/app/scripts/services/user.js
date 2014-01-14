b9f5c1d83112dd6cd4ac4ccfb3d1d7e2d74c072e
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-14 03:53:23 -0800

    [FEATURE] Add the updateSession method to the UserService

--- public/app/scripts/services/user.js
+++ public/app/scripts/services/user.js
@@ -3,17 +3,31 @@ angular.module('publicApp').service('UserService', ['$http','$location', functio
     isLogged: false,
     username: '',
 
+    updateSession: function(fn){
+      $http.get('/api/v1/sessions').success(function(resp){
+        console.log('response')
+        console.log(resp)
+        if (resp.success && resp.session) {
+          console.log('yes')
+          console.log(user)
+          user.isLogged = true
+          user.username = resp.session.username
+          console.log('user',user)
+          $location.path('/gateway_account')
+        }
+      })
+    },
+
     login: function(username, password) {
-      user = this
 			$http.post('/api/v1/sessions', {
 				name: username,
 				password: password
 			}).success(function(response){
         if (!!response.session.username) {
-          this.isLogged = true,
-          this.username = username
+          user.isLogged = true,
+          user.username = username
           console.log('about to redirect to /gateway_account')
-					$location.path('/gatway_account');
+					$location.path('/gateway_account');
 				} else {
 					$location.path('/admin/users/new');
 				}
