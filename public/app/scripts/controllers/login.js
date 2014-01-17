ad59b1428304debc54c536f1ca3c654070a946f7
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-16 17:42:53 -0800

    [FEATURE] Add view, function for registering admin if doesnt exist.

--- public/app/scripts/controllers/login.js
+++ public/app/scripts/controllers/login.js
@@ -2,6 +2,20 @@
 
 angular.module('publicApp')
   .controller('LoginCtrl', ['$scope', '$http', '$location', 'UserService', function ($scope, $http, $location, $user) {
+    function checkAdminExists() {
+      $http.get("/api/v1/gateway/settings").success(function(resp){
+        if (resp.success) {
+          $scope.adminExists = resp.settings.adminExists
+        }
+        $scope.loading = false
+      })
+    }
+
+    checkAdminExists()
+
+    $scope.loading = true
+    $scope.adminExists = false
+
     $scope.user = {}
     $scope.userService = $user
 
