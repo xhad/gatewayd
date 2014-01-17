f5177d9cb426293b7e6206b5e1815e8f1377f0e9
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-17 15:54:02 -0800

    [FEATURE] Add http basic authentication to admin deposits page.

--- public/app/scripts/controllers/admin/gateway/deposits.js
+++ public/app/scripts/controllers/admin/gateway/deposits.js
@@ -1,14 +1,15 @@
 'use strict';
 
 angular.module('publicApp')
-  .controller('AdminDepositsCtrl', ['$scope', '$http', 'UserService', '$location', function ($scope, $http, $user, $location) {
+  .controller('AdminDepositsCtrl', ['$scope', '$http', 'UserService', '$location', 'Base64', function ($scope, $http, $user, $location, Base64) {
     if (!$user.admin) { $location.path('/login') }
 		$scope.deposits = [];
 
 		function getDeposits() {
+      $http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode($user.username+':'+$user.password);
 			$http.get('/api/v1/gateway/deposits')
-			.success(function(deposits) {
-				$scope.deposits = deposits;
+			.success(function(resp) {
+				$scope.deposits = resp.externalTransactions;
 			})
 			.error(function(err) {
 				console.log({ error: err });
@@ -19,10 +20,11 @@ angular.module('publicApp')
 			var bankAccountId = $scope.deposit.bankAccountId;
 			var currency = $scope.deposit.currency;
 			var cashAmount = $scope.deposit.cashAmount;
-			$http.post('/api/v1/gateway/deposits', { 
+      $http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode($user.username+':'+$user.password);
+			$http.post('/api/v1/gateway/transactions/deposit', { 
 				currency: currency, 
 				cashAmount: cashAmount,
-				bankAccountId: bankAccountId
+				accountId: bankAccountId
 			})	
 			.success(function(response){
 				console.log(response);
