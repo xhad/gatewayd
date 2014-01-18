7ae354a32a7b29d09e4b29c80ed0adb164a6a340
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-18 01:45:47 -0800

    [FEATURE] log when a new transaction comes in.

--- listener.js
+++ listener.js
@@ -1,8 +1,8 @@
 var Ripple = require('ripple-lib')
 var request = require('request')
 
-var address = 'rHKueQebtVU9cEamhBbMV8AEViqKjXcBcB';
-var lastTxId = 'E0785E2C6F7A7F84E03B05635A15C4992EA65532D650FF5CB4CE18BA3707DAD8';
+var address = process.env.RIPPLE_ADDRESS || 'rwqhRo41zXZKooNbDNUqQ5dTRrQPQ1BDF7'
+var lastTxId = process.env.LAST_TX_ID || 'E0785E2C6F7A7F84E03B05635A15C4992EA65532D650FF5CB4CE18BA3707DAD8';
 
 function getNextNotification() {
   base = 'http://simple-ripple-listener.herokuapp.com/api/v1/';
@@ -10,7 +10,9 @@ function getNextNotification() {
   request.get({ url: url, json: true }, function(err, resp, body) {
     if (body.notification) {
       lastTxId = body.notification.txHash;
-      handleNewNotification(body.notification)
+      console.log('got a new notification!');
+      console.log(body.notification);
+      handleNewNotification(body.notification);
     } else {
       console.log('no payments, sleep 2 sec');
       setTimeout(getNextNotification, 2000);
