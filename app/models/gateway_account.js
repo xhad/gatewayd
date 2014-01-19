1c84f91a0f9ccb93bafb48f67612cd07ab206ec1
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-18 17:13:11 -0800

    [FEATURE] Add gateway account model methods for ripple address, txns.

--- app/models/gateway_account.js
+++ app/models/gateway_account.js
@@ -1,6 +1,8 @@
 var Sequelize = require('sequelize');
 var db = require('../../config/initializers/sequelize');
 var User = require('../models/user');
+var RippleAddress = require('../models/ripple_address');
+var RippleTransaction = require('../models/ripple_transaction');
 
 var GatewayAccount = sequelize.define('gateway_account', {
   id: { 
@@ -15,15 +17,28 @@ var GatewayAccount = sequelize.define('gateway_account', {
 	}
 }, {
   instanceMethods: {
-    getBalances: function(fn){
-      query = ''
-      query += 'select SUM("cashAmount") as amount, "currency"'
-      query += 'FROM bank_txs GROUP BY "currency"'
-      query += 'WHERE accountId = ?'
-      sequelize.query(query, this.id).complete(function(err, rows){
-        if (err) { fn({ success: false, error: err }); return false }
-        fn({ success: true, balances: rows })
-      })
+    gatewayBalances: function(fn){
+      var query = '';
+      query = 'select SUM("cashAmount") as amount, "currency"';
+      query += 'FROM bank_txs GROUP BY "currency"';
+      query += 'WHERE accountId = ?';
+      sequelize.query(query, this.id).complete(fn);
+    },
+    user: function(fn){
+      var query = 'select * from users where gatewayAccountId = ?';
+      sequelize.query(query, this.id).complete(fn);
+    },
+    rippleAddresses: function(fn){
+      var query = 'select * from ripple_addresses where gatewayAccountId = ?';
+      sequelize.query(query, this.id).complete(fn);
+    },
+    rippleTransactions: function(fn){
+      var query = 'select * from ripple_transactions';
+      query += 'left outer join ripple_transactions' ;
+      query += 'on ripple_transactions.rippleAddressId = ripple_addresses.id'; 
+      query += 'left outer join ripple_addresses';
+      query += 'on ripple_addresses.gatewayAccountId = ?';
+      sequelize.query(query, this.id).complete(fn);
     }
   }
 });
