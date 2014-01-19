256c7588450e5a91c14d4824024fc72632df468b
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-18 19:37:06 -0800

    [FEATURE] Change accountId to externalAccountId in external transactions model

--- app/models/external_transaction.js
+++ app/models/external_transaction.js
@@ -1,10 +1,10 @@
 var sequelize = require('../../config/initializers/sequelize.js');
 
-var ExternalTransaction = sequelize.define('external_transaction', {
+var ExternalTransaction = sequelize.define('external_transactions', {
   id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
   deposit: { type: Sequelize.BOOLEAN, allowNull: false },
   currency: { type: Sequelize.STRING, allowNull: false },
-  accountId: { type: Sequelize.INTEGER, allowNull: false },
+  externalAccountId: { type: Sequelize.INTEGER, allowNull: false },
   cashAmount: { type: Sequelize.DECIMAL, allowNull: false },
   rippleTxId: Sequelize.INTEGER
 });
