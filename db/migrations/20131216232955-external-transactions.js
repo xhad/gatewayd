4152b993f3bf1d80e6349c429c3db89839169fc8
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-18 17:41:41 -0800

    [FEATURE] Add migration files for external transactions and external accounts.

---
+++ db/migrations/20131216232955-external-transactions.js
@@ -0,0 +1,19 @@
+var dbm = require('db-migrate');
+var type = dbm.dataType;
+
+exports.up = function(db, callback) {
+  db.createTable('bank_transactions', { 
+		id: { type: 'int', primaryKey: true, autoIncrement: true },
+    deposit: { type: 'boolean', notNull: true },
+    currency: { type: 'string', notNull: true },
+    cashAmount: { type: 'decimal', notNull: true },
+    accountId: { type: 'int', notNull: true },
+    rippleTxId: { type: 'int'},
+    createdAt: { type: 'datetime', notNull: true },
+    updatedAt: { type: 'datetime' }
+  }, callback);
+};
+
+exports.down = function(db, callback) {
+  db.dropTable('bank_transactions', callback);
+};
