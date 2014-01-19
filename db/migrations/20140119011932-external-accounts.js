280bcb43695406bd2d0deb0c129b004874867484
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-18 19:36:51 -0800

    [FEATURE] Add migration logic for creating external_accounts table.

--- db/migrations/20140119011932-external-accounts.js
+++ db/migrations/20140119011932-external-accounts.js
@@ -2,9 +2,16 @@ var dbm = require('db-migrate');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-
+  db.createTable('external_accounts', {
+    id: { type: 'int', primaryKey: true, autoIncrement: true },	 	
+    name: { type: 'string', notNull: true },
+    gateway_account_id: { type: 'int', notNull: true},
+    createdAt: { type: 'datetime', notNull: true},
+    updatedAt: { type: 'datetime', notNull: true},
+	}, callback);
 };
 
 exports.down = function(db, callback) {
-
+	db.dropTable('external_accounts', callback);
+  callback()
 };
