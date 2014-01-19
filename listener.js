4378d25b3aa5daa21750ee150213e5118c8f8633
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-18 19:40:05 -0800

    [FEATURE] Add function to handle new ripple notifications

--- listener.js
+++ listener.js
@@ -1,8 +1,8 @@
 var Ripple = require('ripple-lib')
 var request = require('request')
 
-var address = process.env.RIPPLE_ADDRESS || 'rwqhRo41zXZKooNbDNUqQ5dTRrQPQ1BDF7'
-var lastTxId = process.env.LAST_TX_ID || 'E0785E2C6F7A7F84E03B05635A15C4992EA65532D650FF5CB4CE18BA3707DAD8';
+var address = process.env.RIPPLE_ADDRESS;
+var lastTxId = process.env.LAST_TX_ID;
 
 function getNextNotification() {
   base = 'http://simple-ripple-listener.herokuapp.com/api/v1/';
@@ -10,19 +10,22 @@ function getNextNotification() {
   request.get({ url: url, json: true }, function(err, resp, body) {
     if (body.notification) {
       lastTxId = body.notification.txHash;
-      console.log('got a new notification!');
-      console.log(body.notification);
       handleNewNotification(body.notification);
+      getNextNotification();
     } else {
-      console.log('no payments, sleep 2 sec');
       setTimeout(getNextNotification, 2000);
     }
   })
 }
 
 function handleNewNotification(notification) {
+  // Inbound
+  // -- Create ripple transaction
+
+  // Outbound
+  // -- Find and update ripple transaction
+
   console.log(notification);
-  getNextNotification();
 }
 
 getNextNotification();
