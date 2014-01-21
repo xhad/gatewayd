e40835aca53c3020a1ceb982f2e505bfd473d1fd
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-21 12:07:40 -0800

    [CHORE] Remove tons of angular views and controllers for simplicity

--- public/app/scripts/controllers/gateway/deposit.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-angular.module('publicApp')
-  .controller('GatewayDepositCtrl', ['$scope', '$http','$location', 'UserService', function ($scope, $http, $location, $user) {
-    if (!$user.isLogged) {
-      $location.path('/login') 
-    }
-  }])
