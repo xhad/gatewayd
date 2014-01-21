387fe28bf03d894ea37483455067c43edb7ef947
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-21 14:13:21 -0800

    [FEATURE] Add user data variables to scope

--- public/app/scripts/controllers/users.js
+++ public/app/scripts/controllers/users.js
@@ -1,17 +1,26 @@
 'use strict';
 
 angular.module('publicApp')
-  .controller('UsersCtrl', ['$scope', '$http', '$location', '$routeParams', 'UserService', function ($scope, $http, $location, $user, $routeParams) {
-    console.log('user id', $routeParams.id);
-    if ($user) {
-      console.log('user is logged in');
+  .controller('UsersCtrl', ['$scope', '$http', '$location', '$route', '$routeParams', 'UserService', function ($scope, $http, $location, $route, $routeParams, $user) {
+    $scope.user = $user;
+    if ($user.isLogged) {
       if (($user.id == $routeParams.id) || $user.admin) {
-        console.log('user is allowed to access this resource');
+        // do ALL THE THINGS!
+        setupDashboard();
+        console.log($scope.user);
       } else {
-        $location.path = '/users/'+$user.id; 
+        $location.path('/users/'+$user.id);
       }
     } else {
-      $location.path = '/login';
+      $location.path('/login');
+    }
+
+    function setupDashboard() {
+      $scope.user.balances = [{ currency: 'USD', amount: 1510 }, { currency: 'XAG', amount: 75 }];
+      $scope.user.ripple_transactions = [];
+      $scope.user.external_transaction = [];
+      $scope.user.external_accounts = [];
+      $scope.user.ripple_addresses = [];
     }
 
   }]);
