26d39dd3926324986dcae725cec814f584256191
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-20 17:17:08 -0800

    [FEATURE] Add issuer columns, organize ripple_transaction model and migration.

--- app/models/ripple_transaction.js
+++ app/models/ripple_transaction.js
@@ -2,18 +2,24 @@ var Sequelize = require('sequelize');
 var db = require('../../config/initializers/sequelize');
 
 module.exports = sequelize.define('ripple_transaction', {
-  id:             { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-  to_currency:     { type: Sequelize.STRING, notNull: true },
-  to_address:      { type: Sequelize.STRING, notNull: true },
-  to_currency:     { type: Sequelize.STRING, notNull: true },
-  to_amount:       { type: Sequelize.DECIMAL, notNull: true },
-  from_currency:   { type: Sequelize.STRING, notNull: true },
-  from_amount:     { type: Sequelize.DECIMAL, notNull: true },
-  from_address:    { type: Sequelize.STRING, notNull: true },
-  issuance:       { type: Sequelize.BOOLEAN, notNull: true },
-  destination_tag: { type: Sequelize.STRING },
-  source_tag:      { type: Sequelize.INTEGER },
-  transaction_hash:         { type: Sequelize.STRING },
-  transaction_state:        { type: Sequelize.STRING }
+
+  id:               { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  ripple_address_id:{ type: Sequelize.INTEGER, notNull: true },
+  transaction_hash: { type: Sequelize.STRING },
+  transaction_state:{ type: Sequelize.STRING },
+
+  to_address:       { type: Sequelize.STRING, notNull: true },
+  to_amount:        { type: Sequelize.DECIMAL, notNull: true },
+  to_currency:      { type: Sequelize.STRING, notNull: true },
+  to_issuer:        { type: Sequelize.STRING, notNull: true },
+
+  from_address:     { type: Sequelize.STRING, notNull: true },
+  from_amount:      { type: Sequelize.DECIMAL, notNull: true },
+  from_currency:    { type: Sequelize.STRING, notNull: true },
+  from_issuer:      { type: Sequelize.STRING, notNull: true },
+
+  destination_tag:  { type: Sequelize.STRING },
+  source_tag:       { type: Sequelize.INTEGER }
+
 })
 
--- db/migrations/20131216233400-ripple-transactions.js
+++ db/migrations/20131216233400-ripple-transactions.js
@@ -3,20 +3,26 @@ var type = dbm.dataType;
 
 exports.up = function(db, callback) {
   db.createTable('ripple_transactions', { 
-		id: { type: 'int', primaryKey: true, autoIncrement: true, notNull: true, unique: true },
-    issuance: { type: 'boolean', notNull: true },
-    to_currency: { type: 'string', notNull: true },
-    to_address: { type: 'string', notNull: true },
-    from_address: { type: 'string', notNull: true },
-    to_amount: { type: 'decimal', notNull: true },
-    from_currency: { type: 'string', notNull: true },
-    from_amount: { type: 'decimal', notNull: true },
-    destination_tag: { type: 'string' },
-    source_tag: { type: 'string' },
+		id:                { type: 'int', primaryKey: true, autoIncrement: true },
+    ripple_address_id: { type: 'int', notNull: true },
     transaction_state: { type: 'string' },
-    transaction_hash: { type: 'string', unique: true },
-    createdAt: { type: 'datetime', notNull: true },
-    updatedAt: { type: 'datetime' }
+    transaction_hash:  { type: 'string', unique: true },
+
+    to_address:        { type: 'string', notNull: true },
+    to_amount:         { type: 'decimal', notNull: true },
+    to_currency:       { type: 'string', notNull: true },
+    to_issuer:         { type: 'string', notNull: true },
+
+    from_address:      { type: 'string', notNull: true },
+    from_amount:       { type: 'decimal', notNull: true },
+    from_currency:     { type: 'string', notNull: true },
+    from_issuer:       { type: 'string', notNull: true },
+
+    destination_tag:   { type: 'string' },
+    source_tag:        { type: 'string' },
+
+    createdAt:         { type: 'datetime', notNull: true },
+    updatedAt:         { type: 'datetime' }
   }, callback);
 };
 
