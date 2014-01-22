a1cb437486b158594d97962370a668a95136be73
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-21 17:46:31 -0800

    [FEATURE] Add shell controller action for getting user balances

--- app/controllers/balances.js
+++ app/controllers/balances.js
@@ -1,10 +1,21 @@
 RippleTransaction = require("../models/ripple_transaction")
 
 module.exports = (function(){
-  return {
-    index: function(req, res){
+  function userIndex(req, res) {
+    User.find(req.params.id).complete(function(err, user) {
+      user.externalBalances(function(err, externalBalances) {
+        user.rippleBalances(function(err, rippleBalances) {
+          res.send({ 
+            external_balances: (externalBalances || []),
+            ripple_balances: (rippleBalances || []) 
+          });
+        })
+      });
+    });
+  } 
 
-    },
+  return {
+    userIndex: userIndex,
 
     gateway: function(req, res) {
       sequelize.query('select SUM("cashAmount") as amount, "currency" FROM bank_txs GROUP BY "currency" WHERE accountId = ?', req.params.accountId).complete(function(err,rows){
