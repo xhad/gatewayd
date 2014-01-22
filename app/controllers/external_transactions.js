4b7577a1e4b4450940243a545cfea4c196caf0e8
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-21 22:01:32 -0800

    [FEATURE] ADd controller action for creating external transactions

--- app/controllers/external_transactions.js
+++ app/controllers/external_transactions.js
@@ -1,44 +1,29 @@
 ExternalTransaction = require("../models/external_transaction.js");
 User = require('../models/user.js');
-handleError = require("../../lib/action_controller").handleError
 
 module.exports = (function(){
-  function forAccount(req, res){
-    ExternalTransaction.findAll({ where: { accountId: req.params.accountId }})
-    .complete(function(err, transactions){
-      if (err) { return handleError(err, res) }
-			res.send({ success: true, gatewayTransactions: transactions })
-    })
-  }
 
-  function createDeposit(req, res) {
-    var accountId = req.body.accountId;
-    var cashAmount = req.body.cashAmount;
-    var currency = req.body.currency;
-    var externalAccountId = req.body.externalAccountId;
-   
-    req.checkBody('accountId', 'invalid accountId').notEmpty();
-    req.checkBody('cashAmount', 'invalid cashAmount').notEmpty();
+  function create(req, res) {
+    req.checkBody('cash_amount', 'invalid cash_amount').notEmpty();
     req.checkBody('currency', 'invalid currency').notEmpty();
-
-    GatewayAccount.find(req.body.account).complete(function(err, account) {
-      if (!err && (req.user.admin || (account.userId == req.user.id))) {
-        ExternalTransaction.create({
-          deposit: true,
-          currency: currency, 
-          cashAmount: cashAmount,
-          accountId: accountId,
-        }).complete(function(err, deposit) {
-          if (err) { res.send({ success: false, error : err }); return } 
-          res.send({ success: true, externalDeposit: deposit });
-        })  
-      } else {
-        res.send({ success: false, error: 'authentication' });
-      }
-    })
+    req.checkBody('external_account_id', 'invalid external_account_id').notEmpty();
+    req.checkBody('deposit', 'invalid deposit boolean').notEmpty();
+    req.checkBody('user_id', 'invalid user_id').notEmpty();
+    if (errors = req.validationErrors()) { 
+      res.send({ error: errors }); return;
+    }
+   
+    ExternalTransaction.create({
+      deposit: req.body.deposit,
+      currency: req.body.currency, 
+      cash_amount: req.body.cash_amount,
+      external_account_id: req.body.external_account_id,
+    }).complete(function(err, transaction) {
+      res.send({ error: err, external_transaction: transaction });
+    });
   };
 
-  function userIndex(req, res) {
+  function index(req, res) {
     User.find(req.params.id).complete(function(err, user) {
       user.externalTransactions(function(err, transactions) {
         res.send({ external_transactions: (transactions || []) });
@@ -46,18 +31,8 @@ module.exports = (function(){
     });
   } 
 
-	function index(req, res) {
-    ExternalTransaction.findAll()
-    .complete(function(err, transactions){
-      if (err) { return handleError(err, res) }
-			res.send({ success: true, gatewayTransactions: transactions })
-    })
-	}
-
   return {
-		userIndex: userIndex,
 		index: index,
-    forAccount: forAccount,
-    createDeposit: createDeposit
+    create: create
 	}
 })();
