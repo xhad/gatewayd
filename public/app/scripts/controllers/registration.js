68a81f118f02b528104861757b26341c1497c6e8
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-21 16:51:28 -0800

    [FEATURE] Add registrations controller for angular.

--- public/app/scripts/controllers/registration.js
+++ public/app/scripts/controllers/registration.js
@@ -1,17 +1,18 @@
 'use strict';
 
 angular.module('publicApp')
-  .controller('GatewayNewUserCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+  .controller('RegistrationCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
 		$scope.user = null;
 
     $scope.registerUser = function () {
-      if ($scope.user.password == $scope.user.password_confirmation) {
-				delete $scope.user.password_confirmation;
-		    $http.post('/api/v1/gateway/users', $scope.user)
-				.success(function(user){
-					updateUsers();
-				})
-				.error(console.log);
-			}
-		}
+      console.log('about to register user');
+      console.log('passwords are the same.');
+      $http.post('/api/v1/gateway/users', $scope.user)
+      .success(function(user){
+        //updateUsers();
+      })
+      .error(function(err) {
+
+      });
+    }
   }]);
