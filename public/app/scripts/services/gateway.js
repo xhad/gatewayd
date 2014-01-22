39be1300dc0f45fabbe9f6d8c5eb1ab5ca4af0e0
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-21 16:55:07 -0800

    [FEATURE] Add gateway angular service for global settings.

---
+++ public/app/scripts/services/gateway.js
@@ -0,0 +1,16 @@
+angular.module('publicApp').service('GatewayService', ['$http', function($http) {
+
+  function updateSettings() {
+    $http.get("/api/v1/gateway/settings").success(function(resp) {
+      console.log('got the settings');
+      settings.hotWallet = resp.settings.hotWallet;
+    });
+  }
+
+  var settings = {
+    hotWallet: false,
+    updateSettings: updateSettings 
+  }
+
+  return settings;
+}])
