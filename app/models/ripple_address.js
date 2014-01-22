9f2e103f4fc10b8f533a4f4b277971a205e0abf7
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-21 16:52:37 -0800

    [FEATURE] Add RippleAddress class method to create a user hosted wallet.

--- app/models/ripple_address.js
+++ app/models/ripple_address.js
@@ -9,11 +9,34 @@ var RippleAddress = sequelize.define('ripple_address', {
 	},
   user_id: { type: Sequelize.INTEGER, notNull: true },
   managed: { type: Sequelize.BOOLEAN, notNull: true, default: false },
-  address: { type: Sequelize.STRING, notNull: true, unique: true },
+  address: { type: Sequelize.STRING, notNull: true },
   secret: { type: Sequelize.STRING },
   tag: { type: Sequelize.STRING },
   type: { type: Sequelize.STRING },
   previous_transaction_hash: { type: Sequelize.STRING }
+}, {
+  classMethods: {
+    createHosted: function(user, fn){
+      console.log('create hosted called');
+      RippleAddress.find({ where: { type: 'hot' }}).complete(function(err, address) {
+        console.log('hot wallet', address);
+        if (address) {
+          var hotWallet = address; 
+          RippleAddress.create({
+            type: 'hosted',
+            managed: true,
+            user_id: user.id,
+            tag: user.id,
+            address: hotWallet.address
+          }).complete(function(err, address){
+            console.log(err);
+            console.log('new ripple address', address);
+            fn(err, address);
+          });
+        } else { fn(err, null) }
+      });
+    }
+  }
 });
 
 module.exports = RippleAddress;
