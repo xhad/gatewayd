42eb3c14aadce713ef52d88381c268970a54a125
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-23 15:50:06 -0800

    [CHORE] Configure server to serve on heroku

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ var express = require('express');
 var childProcess = require('child_process');
 var middleware = require('./config/initializers/middleware.js');
 var router = require('./config/routes.js');
-var host = process.env.HOST || 'localhost';
+var host = process.env.HOST;
 var port = process.env.PORT || 4000;
 var app = express();
 
 middleware.configure(app);
 router.route(app);
 
-app.listen(port, host);
+host ? app.listen(port, host) : app.listen(port);
 childProcess.exec("node listener.js");
 
-console.log('Serving HTTP on', host+":"+port);
+console.log('Serving HTTP on', (host || 'localhost')+":"+port);
 
