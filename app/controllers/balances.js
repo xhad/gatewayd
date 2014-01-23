ed3e82f4911b5e52fd163058ac8c422836e1d58a
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-23 15:26:38 -0800

    [FEATURE] Simplify balances controller to use new user method.

--- app/controllers/balances.js
+++ app/controllers/balances.js
@@ -1,42 +1,16 @@
-RippleTransaction = require("../models/ripple_transaction")
+RippleTransaction = require("../models/ripple_transaction");
+var async = require("async");
 
 module.exports = (function(){
   function userIndex(req, res) {
     User.find(req.params.id).complete(function(err, user) {
-      user.externalBalances(function(err, externalBalances) {
-        user.rippleBalances(function(err, rippleBalances) {
-          res.send({ 
-            external_balances: (externalBalances || []),
-            ripple_balances: (rippleBalances || []) 
-          });
-        })
+      user.externalBalances(function(err, balances) {
+        res.send({ error: err, external_balances: balances });
       });
     });
   } 
 
-  function index(req, res) {
-    var query = 'SELECT to_currency as currency, SUM(to_amount) as amount FROM ripple_transactions';
-    query += ' GROUP BY to_currency';
-    sequelize.query(query).complete(function(err, result){
-      var rippleTransactions = result;
-
-      var query = 'SELECT currency, SUM(cash_amount) as amount FROM external_transactions GROUP BY currency';
-      sequelize.query(query).complete(function(err, external){
-        res.send({ error: err, balances: result.concat(external), external_balances: external });
-      });
-    });
-  }
-
   return {
-    index: index,
-    userIndex: userIndex,
-
-    gateway: function(req, res) {
-      sequelize.query('select SUM("cashAmount") as amount, "currency" FROM bank_txs GROUP BY "currency" WHERE accountId = ?', req.params.accountId).complete(function(err,rows){
-        if (err) { res.send({ success: false, error: err }) }
-        res.send({ success: true, rippleTransactions: rows })
-      })
-    }
-
+    userIndex: userIndex
   }
 })();
