75b476f523d6b93155a87188c9e1706ab59c34ef
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-24 11:57:23 -0800

    [TEST] Add integration test for creating payment using Simple API.

---
+++ test/integration/payment.js
@@ -0,0 +1,28 @@
+var request = require('request');
+
+var payment = {
+  src_address: 'rHKueQebtVU9cEamhBbMV8AEViqKjXcBcB',
+  dst_address: 'r4EwBWxrx5HxYRyisfGzMto3AT8FZiYdWk',
+  dst_amount: {
+    currency: 'USD',
+    value: '0.10',
+    issuer: 'r4EwBWxrx5HxYRyisfGzMto3AT8FZiYdWk'
+  },
+  secret: 'sha6eAz46AziUNVnAYWx3Rp5xReP1'
+};
+
+var params = {
+  url: 'http://localhost:5990/api/v1/addresses/rHKueQebtVU9cEamhBbMV8AEViqKjXcBcB/payments',
+  form: payment,
+  json: true
+};
+
+describe('making a payment', function(){
+  it('should succeed', function(done){
+    request.post(params, function(e,r,body){
+      console.log(body);
+      done();
+    });
+  });
+});
+
