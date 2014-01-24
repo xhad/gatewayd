5f79407cb64d1d85b67eff5b890d144b95c7386f
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-23 21:17:43 -0800

    [FEATURE] Add angular controller function to create ripple withdrawal

--- public/app/scripts/controllers/ripple_transactions.js
+++ public/app/scripts/controllers/ripple_transactions.js
@@ -2,23 +2,32 @@
 
 angular.module('publicApp')
   .controller('RippleTransactionsCtrl', ['$scope', '$http', '$location', '$route', '$routeParams', 'UserService', function ($scope, $http, $location, $route, $routeParams, $user) {
-    $scope.user = $user;
+    if (!$user.isLogged) { $location.path('/login') };
 
-    function createRippleDeposit(opts) {
-      $http.post('/api/v1/ripple_transactions', { form: {
-        deposit: true    
-      }},function(err, transaction) {
+    $scope.user = $user;
+    $scope.rippleWithdrawal = {};
+
+    $scope.createRippleWithdrawal = function() {
+      var opts  = {};
+      opts.from_currency = $scope.rippleWithdrawal.currency;
+      opts.from_amount = parseFloat($scope.rippleWithdrawal.amount);
+      opts.from_issuer = 'r4EwBWxrx5HxYRyisfGzMto3AT8FZiYdWk';
+      opts.from_address = 'r4EwBWxrx5HxYRyisfGzMto3AT8FZiYdWk';
+
+      opts.to_currency = $scope.rippleWithdrawal.currency;
+      opts.to_amount = parseFloat($scope.rippleWithdrawal.amount);
+      opts.to_issuer = 'r4EwBWxrx5HxYRyisfGzMto3AT8FZiYdWk';
+      opts.to_address = 'r4EwBWxrx5HxYRyisfGzMto3AT8FZiYdWk';
+      opts.issuance = true;
+
+      opts.to_address = $scope.rippleWithdrawal.ripple_address;
+      opts.from_address = $scope.rippleWithdrawal.ripple_address;
+
+      opts.ripple_address_id = 3;
+      
+      $http.post('/api/v1/users/'+$user.id+'/ripple_transactions', opts, function(err, transaction) {
         console.log(err); 
         console.log(transaction);
       });
     }
-
-    if ($user.isLogged) {
-
-    } else {
-      $location.path('/login');
-    }
-
-    //createRippleDeposit({});
-
   }]);
