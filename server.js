a0f2200aa26ecff7716f4dc374ed831810386ee1
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-24 14:59:35 -0800

    [FEATURE] Add outgoing payment worker as child process.

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ middleware.configure(app);
 router.route(app);
 
 host ? app.listen(port, host) : app.listen(port);
-childProcess.exec("node listener.js");
 childProcess.exec("node ripple-simple/app.js"); // http://0.0.0.0:5990
+childProcess.exec("node workers/outgoing_ripple_payments.js");
+childProcess.exec("node listener.js");
 
 console.log('Serving HTTP on', (host || 'localhost')+":"+port);
 
