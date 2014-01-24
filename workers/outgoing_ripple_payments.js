b3a42f0e597d8d1e6af78ae1c9958c0ea4f013ac
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-24 14:19:42 -0800

    [FEATURE] Monitor new ripple transactions and submit payments.

--- workers/outgoing_ripple_payments.js
+++ workers/outgoing_ripple_payments.js
@@ -1,18 +1,58 @@
 var RippleTransaction = require('../app/models/ripple_transaction');
+var RippleAddress = require('../app/models/ripple_address');
+var request = require('request');
+var hotWallet = null;
 
 function popCreatedRippleTransaction() {
-  RippleTransaction.find({ where: { transaction_state: 'created' }}).complete(function(err, transaction){
+  RippleTransaction.find({ where: [
+    "transaction_state IS NULL"
+  ]}).complete(function(err, transaction){
     if (transaction) {
-      console.log('found a new transaction to send out');
-      console.log('go ahead and send it!');
+      var payment = {
+        src_address: hotWallet.address,
+        dst_address: transaction.to_address,
+        dst_amount: {
+          currency: transaction.to_currency,
+          value: transaction.to_amount,
+          issuer: transaction.to_issuer
+        },
+        secret: hotWallet.secret
+      };
+
+      var params = {
+        url: 'http://localhost:5990/api/v1/addresses/'+hotWallet.address+'/payments',
+        form: payment,
+        json: true
+      };
+
+      request.post(params, function(err,resp,body){
+        if (body.success) {
+          console.log("success", body);
+          transaction.transaction_state = 'submitted';
+          transaction.transaction_hash = body.confirmation_token;
+        } else {
+          transaction.transaction_state = body.error;
+        }
+        transaction.save().complete(function(err, transaction){
+          console.log('saved');
+          console.log(transaction);
+          setTimeout(popCreatedRippleTransaction, 1000);
+        });
+      });
     } else {
       console.log('no new transactions to send out');
+      setTimeout(popCreatedRippleTransaction, 1000);
     }
-    setTimeout(popCreatedRippleTransaction, 1000);
   });
 }
 
-popCreatedRippleTransaction();
+function getHotWallet(fn) {
+  RippleAddress.find({ where: { type: 'hot' }}).complete(fn);
+}
 
-
+getHotWallet(function(err, wallet) {
+  console.log('got the hot wallet', wallet.secret);
+  hotWallet = wallet;
+  popCreatedRippleTransaction();
+});
 
