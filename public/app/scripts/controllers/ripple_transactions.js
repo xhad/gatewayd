fd705cc454cca82da24d970ce01502e43fa27d8f
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-29 02:04:13 -0800

    [FEATURE] Get ripple address before creating ripple transaction.

--- public/app/scripts/controllers/ripple_transactions.js
+++ public/app/scripts/controllers/ripple_transactions.js
@@ -13,25 +13,22 @@ angular.module('publicApp')
       var opts  = {};
       opts.from_currency = $scope.rippleWithdrawal.currency;
       opts.from_amount = parseFloat($scope.rippleWithdrawal.amount);
-      opts.from_issuer = 'r4EwBWxrx5HxYRyisfGzMto3AT8FZiYdWk';
-      opts.from_address = 'r4EwBWxrx5HxYRyisfGzMto3AT8FZiYdWk';
+      opts.from_issuer = $gateway.hotWallet;
+      opts.from_address = $gateway.hotWallet;
 
       opts.to_currency = $scope.rippleWithdrawal.currency;
       opts.to_amount = parseFloat($scope.rippleWithdrawal.amount);
-      opts.to_issuer = 'r4EwBWxrx5HxYRyisfGzMto3AT8FZiYdWk';
-      opts.to_address = 'r4EwBWxrx5HxYRyisfGzMto3AT8FZiYdWk';
+      opts.to_issuer = $gateway.hotWallet;
       opts.issuance = true;
 
       opts.to_address = $scope.rippleWithdrawal.ripple_address;
-      opts.from_address = $scope.rippleWithdrawal.ripple_address;
 
-      opts.ripple_address_id = 3;
-      
-
-      $http.post('/api/v1/users/'+$user.id+'/ripple_transactions', opts).success(function(err, transaction) {
-        console.log(err);
-        console.log(transaction);
+      $http.get('/api/v1/users/'+$user.id+'/ripple_addresses').success(function(res){
+        opts.ripple_address_id = res.ripple_addresses[0].id;
+        $http.post('/api/v1/users/'+$user.id+'/ripple_transactions', opts).success(function(err, transaction) {
+          $location.path('/users/'+$user.id);
+        });
+        
       });
-      $location.path('/users/'+$user.id);
     }
   }]);
