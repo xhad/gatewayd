8eba4b4e93a1ba676ea142849db61a3d37a73760
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-01 15:06:12 -0800

    [FEATURE] Add nconf to server.js, with config file path and defaults

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ var host = process.env.HOST;
 var port = process.env.PORT || 4000;
 var app = express();
 var sequelize = require('./config/initializers/sequelize.js');
+var fs = require('fs');
+var nconf = require("nconf");
+
+nconf
+  .env()
+  .file({ file: './config/gateway.json' });
+
+nconf.defaults({
+  'RIPPLE_REST_API': 'http://localhost:5990',
+  'DATABASE_URL': 'postgres://username:password@localhost/ripple_gateway'
+});
 
 middleware.configure(app);
 router.route(app);
