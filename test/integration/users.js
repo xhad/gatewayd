3119a57d27d12437a3b42868195a3be3df77ff80
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-04 18:45:43 -0800

    [TEST] Add five empty integration tests for User

---
+++ test/integration/users.js
@@ -0,0 +1,28 @@
+describe('Users', function() {
+
+  it.skip('should create a new user given a name and password', function(done){
+    // POST /users
+    done();
+  });
+
+  it.skip("should verify a user's credentials given a name and password", function(done){
+    // GET /users
+    done();
+  });
+
+  it.skip('should not allow the creation of two users with the same name', function(done){
+    // POST /users
+    done();
+  });
+
+  it.skip('should not allow the creation of a user with the name "admin"', function(done){
+    // POST /users
+    done();
+  });
+
+  it.skip('should allow the admin user to verify their credentials', function(done){
+    // GET /users
+    done();
+  });
+
+});
