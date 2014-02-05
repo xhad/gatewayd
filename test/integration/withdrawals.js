7eb9e48c88593c2ff4eb49e2aede28fac658f614
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-04 19:49:15 -0800

    [TEST ] Add five empty integrations tests for withdrawals.

---
+++ test/integration/withdrawals.js
@@ -0,0 +1,37 @@
+
+describe('Withdrawals', function(){
+  
+  describe('user', function(){
+
+    it.skip('should create a withdrawal from a hosted wallet on behalf of a user', function(){
+      // POST /withdrawals  
+      done();
+    });
+
+    it.skip('should list withdrawals  made from a hosted wallet on behalf of a user', function(){
+      // GET /withdrawals
+      done();
+    });
+
+  });
+
+  describe('admin', function(){
+
+    it.skip('should create a withdrawal from a hosted wallet given a user', function(){
+      // POST /withdrawals 
+      done();
+    });
+
+    it.skip('should create a withdrawal from a hosted wallet given a user', function(){
+      // GET /withdrawals
+      done();
+    });
+
+    it.skip('should list all withdrawals  made from all hosted wallets', function(){
+      // GET /withdrawals
+      done();
+    });
+
+  }); 
+
+});
