9a4bc131491d60d75b89b269939464a479ee54bb
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-04 20:12:11 -0800

    [TEST] Add empty integration tests for ripple_transactions

---
+++ test/integration/ripple_transactions.js
@@ -0,0 +1,42 @@
+describe('RippleTransactions', function(){
+  
+  describe('on behalf of a user', function(){
+
+    it.skip('should create a payment to a ripple account', function(){
+      // POST /payments  
+      done();
+    });
+
+    it.skip('should not be able to send a payment to the gateway', function(){
+      // POST /payments  
+      done();
+    });
+
+    it.skip('should list all payments made to or from a user', function(){
+      // GET /payments
+      done();
+    });
+
+  });
+
+  describe('on behalf of an admin', function(){
+
+    it.skip('should create payment to a ripple account', function(){
+      // POST /payments 
+      done();
+    });
+
+    it.skip('should list payments sent to or from a hosted wallet', function(){
+      // GET /payments
+      done();
+    });
+
+    it.skip('should list all payments made to or from all hosted wallets', function(){
+      // GET /payments
+      done();
+    });
+
+  }); 
+
+});
+
