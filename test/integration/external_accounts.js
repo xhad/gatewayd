3d4c5243caea24c3d93d5cd296f6f69c4ebe913f
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-04 19:08:07 -0800

    [TEST] Add five pending external accounts tests.

---
+++ test/integration/external_accounts.js
@@ -0,0 +1,37 @@
+
+describe('External Accounts', function(){
+  
+  describe('user', function(){
+
+    it.skip('should create an external account on behalf of a user', function(done){
+      // POST /external_accounts  
+      done();
+    });
+
+    it.skip('should list external accounts of a user for that user', function(done){
+      // GET /external_accounts  
+      done();
+    });
+
+  });
+
+  describe('admin', function(){
+
+    it.skip('should create an external account given a user', function(){
+      // POST /external_accounts 
+      done();
+    });
+
+    it.skip('should list external accounts of a user given a user', function(){
+      // POST /external_accounts 
+      done();
+    });
+
+    it.skip('should list all external accounts of all users', function(){
+      // POST /external_accounts 
+      done();
+    });
+
+  }); 
+
+});
