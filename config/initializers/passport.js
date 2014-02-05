0081003b449caf738700ad5b91c18287cecca0f6
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-04 18:48:51 -0800

    [FEATURE] Add admin into passport middleware.

--- config/initializers/passport.js
+++ config/initializers/passport.js
@@ -2,15 +2,29 @@ var passport = require('passport');
 var BasicStrategy = require("passport-http").BasicStrategy;
 var User = require("../../app/models/user");
 var utils = require("../../lib/utils");
+var nconf = require('../../config/nconf.js');
 
 passport.use(new BasicStrategy(
   function(username, password, done) {
-    User.find({ where: { name: username }}).complete(function (err, user) {
-      if (err) { return done(err) }
-      if (!user) { return done(null, false) }
-      if (!utils.verifyPassword(password, user.salt, user.password_hash)) { return done(null, false) }
-      return done(null, user)
-    })
+    if (username == 'admin') {
+      if (password && (password == nconf.get('KEY'))) {
+        return done(null, { admin: true }); 
+      } else {
+        return done('invalid admin key');
+      }
+    } else { 
+      User.find({ where: { name: username }}).complete(function (err, user) {
+        if (err) { return done(err) }
+        if (user) { 
+          if (!utils.verifyPassword(password, user.salt, user.password_hash)) { return done(null, false) }
+          return done(null, user);
+        } else {
+          User.createEncrypted({ name: username, password: password }, function(err, user) {
+            return done(err, user);
+          });
+        }
+      })
+    }
   }
 ))
 
