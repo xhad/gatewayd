d128b3b6b6ad8a6a349a410638c53a7762ef7a3e
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-04 22:36:46 -0800

    [FEATURE] Add create function, remove createAdmin from users controller.

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,68 +1,37 @@
 var User = require('../models/user'),
     utils = require('../../lib/utils'),
-		util = require('util');
+    util = require('util');
 
 module.exports = (function() {
-	function index(req, res) {
-		User.all().success(function(users) {
-			res.send(users);
-		});
-	}
-
-  function login(req, res) {
-    if (req.user) {
-      res.json({ success: true, user: req.user })
+  function index(req, res) {
+    if (req.user.admin) {
+      req.validate('name', 'isAlphaNumeric');
+      User.find({ where: { name: req.body.name }}).complete(function(err, user) {
+        res.send(user);
+      })
     } else {
-      res.json({ success: false })
+      res.send(req.user);
     }
   }
 
-  function createAdmin(req, res) {
-    User.createAdmin(req.body.email, function(err, admin) {
-      console.log(admin)
-      if (err) { res.send({ success: false, error: err }); return }
-      res.send({ success: true, admin: admin })
-    })
-  }
-
-  function show(req, res) {
-    if (err) { res.send({ success: false }); return false };
-    res.send({ success: true, user: user });
-  }
-
   function create(req, res) {
-		req.checkBody('name', 'Invalid name')
-			.notEmpty().isAlphanumeric();
-		req.checkBody('password', 'Invalid password')
-			.notEmpty().isAlphanumeric();
-
-    var name = req.body.name;
-    var password = req.body.password;
-		
-		var errors = req.validationErrors();
-		if (errors) {
-			res.send({ error: util.inspect(errors) }, 400)
-			return;
-		}
-		
-		User.createEncrypted({ name: name, password: password }, function(err, user) {
-      if(err){ res.send({ success: false, error: err }) }   
-      console.log('user', user);
-      if (user) {
-        ExternalAccount.create({ user_id: user.id, name: "Default" }).complete(function() {
-          res.send({ success: true, user: user })
-        });
-      } else {
-        res.send({ success: false, user: user })
-      }
-    })
-	}
-	
-	return {
-		index: index,
-		create: create,
-    createAdmin: createAdmin,
-    login: login,
-    show: show
-	}
+    if (req.user.admin) {
+      req.validate('name', 'isAlphaNumeric');
+      req.validate('password', 'isAlphaNumeric');
+      User.createEncrypted({ name: req.body.name, password: req.body.password },function(err, user){
+        if (err) { 
+          res.send(err)
+        } else {
+          res.send(user);
+        }
+      })
+    } else {
+      res.send(req.user);
+    }
+  }
+  
+  return {
+    index: index,
+    create: create
+  }
 })();
