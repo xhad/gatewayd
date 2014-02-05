83c3acd91ed1e78b33325cf9baa4e547ef6d3b08
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-05 15:10:04 -0800

    [FEATURE] Default SSL config var to true.

--- config/nconf.js
+++ config/nconf.js
@@ -6,7 +6,8 @@ nconf
 
 nconf.defaults({
   'RIPPLE_REST_API': 'http://localhost:5990',
-  'DATABASE_URL': 'postgres://username:password@localhost:5432/ripple_gateway'
+  'DATABASE_URL': 'postgres://username:password@localhost:5432/ripple_gateway',
+  'SSL': true
 });
 
 module.exports = nconf;
