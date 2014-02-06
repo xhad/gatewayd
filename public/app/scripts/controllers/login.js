9fa71db41e5d48108221c353bfb34ad3b49e93ca
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-05 20:34:19 -0800

    [FEATURE] Remove function to check if admin exists in login page.

--- public/app/scripts/controllers/login.js
+++ public/app/scripts/controllers/login.js
@@ -2,23 +2,8 @@
 
 angular.module('publicApp')
   .controller('LoginCtrl', ['$scope', '$http', '$location', 'UserService', function ($scope, $http, $location, $user) {
-    function checkAdminExists() {
-      $http.get("/api/v1/settings").success(function(resp){
-        if (resp.success) {
-          $scope.adminExists = resp.settings.adminExists
-        }
-        $scope.loading = false
-      })
-    }
-
-    checkAdminExists()
-
-    $scope.loading = true
-    $scope.adminExists = false
-    $scope.admin = { email: '' }
-
     $scope.user = {}
-    $scope.userService = $user
+    $scope.userService = $user;
 
     $scope.logout = function() {
       $user.logout()
