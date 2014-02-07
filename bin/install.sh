1c6f3f8f39e75510acc6e858717fe47c67c0bdad
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-06 18:07:33 -0800

    [BUG] Install libpq-dev not libpg-dev.

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,7 @@ sudo apt-get -y install python-software-properties
 sudo apt-get -y install python
 sudo apt-get -y install g++
 sudo apt-get -y install make
-sudo apt-get -y install libpg-dev
+sudo apt-get -y install libpq-dev
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get -y update
 sudo apt-get -y install nodejs
