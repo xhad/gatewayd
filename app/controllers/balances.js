33a3a86f224f9c75bf5d1cbadf2444eb71c84db7
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-07 14:14:05 -0800

    [FEATURE] Rename userIndex controller action to index.

--- app/controllers/balances.js
+++ app/controllers/balances.js
@@ -2,13 +2,13 @@ RippleTransaction = require("../models/ripple_transaction");
 var async = require("async");
 
 module.exports = (function(){
-  function userIndex(req, res) {
+  function index(req, res) {
     req.user.balances(function(err, balances) {
       res.send({ error: err, balances: balances });
     });
   } 
 
   return {
-    userIndex: userIndex
+    index: index
   }
 })();
