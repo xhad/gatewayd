1f7e71cd894791e33534d7cdece2d39819870ff6
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-12 15:59:37 -0800

    [FEATURE] Add deposit, withdraw instance methods to external_account.

--- app/models/external_account.js
+++ app/models/external_account.js
@@ -1,10 +1,26 @@
 var db = require('../../config/initializers/sequelize.js');
+var ExternalTransaction = require('./external_transaction');
 var Sequelize = require('sequelize');
 
 var ExternalAccount = db.define('external_accounts', {
   id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
   name: { type: Sequelize.STRING, allowNull: false },
   user_id: { type: Sequelize.INTEGER, allowNull: false },
+}, {
+  instanceMethods: {
+    deposit: function(opts, fn) {
+      opts.external_account_id = this.id;
+      opts.deposit = true;
+      opts.amount = parseFloat(opts.amount);
+      ExternalTransaction.create(opts).complete(fn);
+    },
+    withdraw: function(opts, fn) {
+      opts.external_account_id = this.id;
+      opts.deposit = false;
+      opts.amount = parseFloat(opts.amount);
+      ExternalTransaction.create(opts).complete(fn);
+    }
+  } 
 });
 
 module.exports = ExternalAccount;
--- app/models/external_transaction.js
+++ app/models/external_transaction.js
@@ -3,10 +3,10 @@ var Sequelize = require("sequelize");
 
 var ExternalTransaction = db.define('external_transactions', {
   id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-  deposit: { type: Sequelize.BOOLEAN, allowNull: false },
+  amount: { type: Sequelize.DECIMAL, allowNull: false },
   currency: { type: Sequelize.STRING, allowNull: false },
+  deposit: { type: Sequelize.BOOLEAN, allowNull: false },
   external_account_id: { type: Sequelize.INTEGER, allowNull: false },
-  cash_amount: { type: Sequelize.DECIMAL, allowNull: false },
   ripple_transaction_id: Sequelize.INTEGER
 });
 
