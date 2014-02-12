8d5ea01c20eff5dcb344e51cfc422236c95ad789
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-11 23:28:03 -0800

    [TEST] Add example test for sending payment.

--- test/integration/ripple_transactions.js
+++ test/integration/ripple_transactions.js
@@ -7,17 +7,42 @@ describe('RippleTransactions', function(){
   
   describe('on behalf of a user', function(){
 
-    it.skip('should create a payment to a ripple account', function(){
+    before(function(done){
+      client = new RippleGateway.Client({
+        api: 'https://localhost:4000'
+      });
+      client.user = rand();
+      client.secret = rand();
+      client.createUser({}, function(err, resp){
+        if (err) { throw new Error(err); }
+        user = resp;
+        done();
+      });
+    });
+
+    it('should create a payment to a ripple account', function(done){
+      var paymentOptions = {
+        to_account: 'rNeSkJhcxDaqzZCAvSfQrxwPJ2Kjddrj4a',
+        from_account: 'rNeSkJhcxDaqzZCAvSfQrxwPJ2Kjddrj4a',
+        from_tag: '2',
+        amount: '1',
+        currency: 'USD'
+      };
+      client.sendPayment(paymentOptions, function(err, payment){
+        console.log(err, payment);
+        assert(!err);
+        assert(payment.id > 0);
+        done();
+      });
+    });
+
+    it('should not be able to send a payment to the gateway', function(done){
       // POST /payments  
+      assert(true);
       done();
     });
 
-    it.skip('should not be able to send a payment to the gateway', function(){
-      // POST /payments  
-      done();
-    });
-
-    it.skip('should list all payments made to or from a user', function(){
+    it.skip('should list all payments made to or from a user', function(done){
       // GET /payments
       done();
     });
@@ -26,17 +51,17 @@ describe('RippleTransactions', function(){
 
   describe('on behalf of an admin', function(){
 
-    it.skip('should create payment to a ripple account', function(){
+    it.skip('should create payment to a ripple account', function(done){
       // POST /payments 
       done();
     });
 
-    it.skip('should list payments sent to or from a hosted wallet', function(){
+    it.skip('should list payments sent to or from a hosted wallet', function(done){
       // GET /payments
       done();
     });
 
-    it.skip('should list all payments made to or from all hosted wallets', function(){
+    it.skip('should list all payments made to or from all hosted wallets', function(done){
       // GET /payments
       done();
     });
