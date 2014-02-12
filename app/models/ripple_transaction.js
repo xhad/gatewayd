f0e041b7b84220fd3532b3a6ef82ef9371ba95b3
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-12 15:30:25 -0800

    [FEATURE] Add to_address_id, from_address_id to ripple_transaction model, table.

--- app/models/ripple_transaction.js
+++ app/models/ripple_transaction.js
@@ -1,26 +1,17 @@
 var db = require('../../config/initializers/sequelize');
 var Sequelize = require("sequelize");
 
-module.exports = db.define('ripple_transaction', {
-
+module.exports = db.define('ripple_transactions', {
   id:               { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-  ripple_address_id:{ type: Sequelize.INTEGER, notNull: true },
-  transaction_hash: { type: Sequelize.STRING },
+  to_address_id:    { type: Sequelize.INTEGER, notNull: true },
+  from_address_id:  { type: Sequelize.INTEGER, notNUll: true },
   transaction_state:{ type: Sequelize.STRING },
-  issuance:         { type: Sequelize.BOOLEAN, notNull: true},
-
-  to_address:       { type: Sequelize.STRING, notNull: true },
+  transaction_hash: { type: Sequelize.STRING },
   to_amount:        { type: Sequelize.DECIMAL, notNull: true },
   to_currency:      { type: Sequelize.STRING, notNull: true },
   to_issuer:        { type: Sequelize.STRING, notNull: true },
-
-  from_address:     { type: Sequelize.STRING, notNull: true },
   from_amount:      { type: Sequelize.DECIMAL, notNull: true },
   from_currency:    { type: Sequelize.STRING, notNull: true },
-  from_issuer:      { type: Sequelize.STRING, notNull: true },
-
-  destination_tag:  { type: Sequelize.STRING },
-  source_tag:       { type: Sequelize.INTEGER }
-
+  from_issuer:      { type: Sequelize.STRING, notNull: true }
 })
 
--- db/migrations/20131216233400-ripple-transactions.js
+++ db/migrations/20131216233400-ripple-transactions.js
@@ -4,23 +4,19 @@ var type = dbm.dataType;
 exports.up = function(db, callback) {
   db.createTable('ripple_transactions', { 
 		id:                { type: 'int', primaryKey: true, autoIncrement: true },
-    ripple_address_id: { type: 'int', notNull: true },
+    to_address_id:     { type: 'int', notNull: true },
+    from_address_id:   { type: 'int', notNull: true },
+
     transaction_state: { type: 'string' },
     transaction_hash:  { type: 'string', unique: true },
-    issuance:          { type: 'string' },
 
-    to_address:        { type: 'string', notNull: true },
     to_amount:         { type: 'decimal', notNull: true },
     to_currency:       { type: 'string', notNull: true },
     to_issuer:         { type: 'string', notNull: true },
 
-    from_address:      { type: 'string', notNull: true },
     from_amount:       { type: 'decimal', notNull: true },
     from_currency:     { type: 'string', notNull: true },
     from_issuer:       { type: 'string', notNull: true },
-
-    destination_tag:   { type: 'string' },
-    source_tag:        { type: 'string' },
 
     createdAt:         { type: 'datetime', notNull: true },
     updatedAt:         { type: 'datetime' }
