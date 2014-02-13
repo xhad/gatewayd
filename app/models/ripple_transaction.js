77de3bfe5d8e08e8be5d37d8f282d7dcece47187
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-13 10:37:33 -0800

    [FEATURE] Add validations to ripple_transactions model.

--- app/models/ripple_transaction.js
+++ app/models/ripple_transaction.js
@@ -2,16 +2,48 @@ var db = require('../../config/initializers/sequelize');
 var Sequelize = require("sequelize");
 
 module.exports = db.define('ripple_transactions', {
-  id:               { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-  to_address_id:    { type: Sequelize.INTEGER, notNull: true },
-  from_address_id:  { type: Sequelize.INTEGER, notNUll: true },
-  transaction_state:{ type: Sequelize.STRING },
-  transaction_hash: { type: Sequelize.STRING },
-  to_amount:        { type: Sequelize.DECIMAL, notNull: true },
-  to_currency:      { type: Sequelize.STRING, notNull: true },
-  to_issuer:        { type: Sequelize.STRING, notNull: true },
-  from_amount:      { type: Sequelize.DECIMAL, notNull: true },
-  from_currency:    { type: Sequelize.STRING, notNull: true },
-  from_issuer:      { type: Sequelize.STRING, notNull: true }
+  id: { 
+    type: Sequelize.INTEGER, 
+    primaryKey: true, 
+    autoIncrement: true 
+  },
+  to_address_id: { 
+    type: Sequelize.INTEGER, 
+    validate: { notNull: true }
+  },
+  from_address_id: { 
+    type: Sequelize.INTEGER, 
+    validate: { notNull: true }
+  },
+  to_amount: { 
+    type: Sequelize.DECIMAL, 
+    validate: { notNull: true }
+  },
+  to_currency: { 
+    type: Sequelize.STRING, 
+    validate: { notNull: true }
+  },
+  to_issuer: { 
+    type: Sequelize.STRING, 
+    validate: { notNull: true }
+  },
+  from_amount: { 
+    type: Sequelize.DECIMAL, 
+    validate: { notNull: true }
+  },
+  from_currency: { 
+    type: Sequelize.STRING, 
+    validate: { notNull: true }
+  },
+  from_issuer: { 
+    type: Sequelize.STRING, 
+    validate: { notNull: true }
+  },
+  transaction_state:{ 
+    type: Sequelize.STRING 
+  },
+  transaction_hash: { 
+    type: Sequelize.STRING 
+  }
 })
 
