6c523dbf452ae2a0255c2c439a6cc25718f79644
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-18 17:07:51 -0800

    [TEST] Add API level test descriptions for hosted balances.

---
+++ test/api/balances.js
@@ -0,0 +1,20 @@
+
+describe('Hosted Wallet Balances', function(){
+
+  it('should be able to retrive all the balances with a user id', function(fn) {
+
+  });
+
+  it('should be able to retrieve all balances of a particular currency/issuer', function(fn) {
+
+  });
+
+  it('should retrieve all balances of a particular currency/issuer and user id combination', function(fn){
+
+  });
+
+  it('should be able to retrieve a single balance given its id', function(fn){
+
+  });
+
+});
