6d58d6a0e15145299da87da46078f60e8234496a
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-26 15:55:56 -0800

    [FEATURE] Default port to 5000.

--- config/nconf.js
+++ config/nconf.js
@@ -7,7 +7,8 @@ nconf
 nconf.defaults({
   'RIPPLE_REST_API': 'http://localhost:5990',
   'DATABASE_URL': 'postgres://postgres:password@localhost:5432/ripple_gateway',
-  'SSL': true
+  'SSL': true,
+  'PORT': 5000
 });
 
 module.exports = nconf;
