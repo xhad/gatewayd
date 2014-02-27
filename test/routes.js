785fd8ae92bcf4a304bbf4bc97949324e6eef6cf
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-26 16:03:51 -0800

    [CHORE] Remove unused test files.

--- test/routes.js
+++
@@ -1,9 +0,0 @@
-var router = require('../config/routes.js');
-
-describe('routing urls to controller actions', function(){
-  it("should configure all the routes", function(fn){
-    router.configure(app, passport);
-    // assert that all the routes call the controllers
-    fn();
-  });
-});
