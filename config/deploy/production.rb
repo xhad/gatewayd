0be036482c3d581e1ae3ace5a3c5c31797dddb99
Steven Zeiler <zeiler.steven@gmail.com>
2014-03-16 16:16:31 -0700

    [FEATURE] Configure capistrano to deploy app.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -26,24 +26,20 @@ set :user, 'ubuntu'
 role :app, "ubuntu@ec2-54-242-187-121.compute-1.amazonaws.com"
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+  task :npm_install, [:roles] => :app do
+    on roles(:app) do
+      execute "cd #{release_path} && npm install"
     end
   end
 
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  task :copy_config do
+    on roles(:app) do
+      execute "ln -s #{shared_path}/config.json #{release_path}/config/config.json"
     end
   end
-
 end
+
+after "deploy:updated", "deploy:cleanup"
+after "deploy:cleanup", "deploy:copy_config"
+after "deploy:finished", "deploy:npm_install"
+
