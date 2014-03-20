6f1681f6ebdddd14be9248861e519def10106f46
Steven Zeiler <zeiler.steven@gmail.com>
2014-03-20 11:22:52 -0700

    [CHORE] Remove ssh from ripple rest api endpoint.

--- config/nconf.js
+++ config/nconf.js
@@ -5,7 +5,7 @@ nconf
   .env();
 
 nconf.defaults({
-  'RIPPLE_REST_API': 'https://localhost:5990',
+  'RIPPLE_REST_API': 'http://localhost:5990',
   'DATABASE_URL': 'postgres://postgres:password@localhost:5432/ripple_gateway',
   'RIPPLE_DATAMODEL_ADAPTER': 'ripple-gateway-data-sequelize-adapter',
   'RIPPLE_EXPRESS_GATEWAY': 'ripple-gateway-express',
