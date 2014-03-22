c8919d53bd3109e2403417a4a8b83ac731319c8e
Steven Zeiler <zeiler.steven@gmail.com>
2014-03-22 14:35:18 -0700

    [FEATURE] Record incoming ripple payments and withdraw them.

--- workers/incoming.js
+++ workers/incoming.js
@@ -1,12 +1,25 @@
 var Listener = require('../lib/listener.js');
 var nconf = require('../config/nconf.js');
+var abstract = require('../lib/abstract.js');
 
 var listener = new Listener();
 
 listener.onPayment = function(payment) {
-  
-  // do what you want with the payment here
-  console.log(payment);
+  if (payment.destination_account == nconf.get('gateway_cold_wallet')) {
+    var dt = payment.destination_tag;
+    if (dt){
+      var amount = payment.destination_amount.value;
+      var currency = payment.destination_amount.currency;
+      var issuer = payment.destination_amount.issuer;
+      if (issuer == nconf.get('gateway_cold_wallet')) {
+
+        abstract.recordIncomingPayment(dt, currency, amount, function(err, record) {
+          consle.log(err, record.toJSON()); 
+        });
+
+      }
+    }
+  };
 
 };
 
