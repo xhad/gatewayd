626f36965fb089e929d24ab5025c03a3ae8a4a39
Steven Zeiler <zeiler.steven@gmail.com>
2014-03-27 17:58:27 -0700

    [FEATURE] Build payment first in outgoing.js process.

--- processes/outgoing.js
+++ processes/outgoing.js
@@ -1,6 +1,7 @@
 var api = require('ripple-gateway-data-sequelize-adapter');
 var send = require("../lib/send_payment");
 var nconf = require("../config/nconf");
+var build_payment = require('../lib/build_payment');
 
 process.env.DATABASE_URL = nconf.get('DATABASE_URL');
 
@@ -12,18 +13,26 @@ function workJob() {
       if (transaction) {
         api.rippleAddresses.read(transaction.to_address_id, function(err, address) {
           console.log(address.address, transaction.to_currency + transaction.to_issuer)
-          send(address.address, transaction.to_amount, transaction.to_currency, null, function(err, resp){
-            if (err) { setTimeout(workJob, 500); return }
-            var payment = JSON.parse(resp);
-            if (payment.success) {
-              transaction.transaction_state = 'sent';
+          build_payment(address.address, transaction.to_amount, transaction.to_currency, function(err, payment) {
+            if (err) { 
+              transaction.transaction_state = 'error';
               transaction.save().complete(function(){
                 setTimeout(workJob, 500);
               });
-            } else {
-              setTimeout(workJob, 500);
+              return;
             }
-          });
+            send(payment, function(err, payment){
+              if (err) { setTimeout(workJob, 500); return }
+              if (payment.success) {
+                transaction.transaction_state = 'sent';
+                transaction.save().complete(function(){
+                  setTimeout(workJob, 500);
+                });
+              } else {
+                setTimeout(workJob, 500);
+              }
+            });
+          });          
         });
       } else {
         setTimeout(workJob, 500);
