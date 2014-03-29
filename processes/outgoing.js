e11a7eda59d5e5e596240f460287209484bfb675
Steven Zeiler <zeiler.steven@gmail.com>
2014-03-28 19:11:37 -0700

    [FEATURE] Use gateway listOutgoingPayments function in worker.

--- processes/outgoing.js
+++ processes/outgoing.js
@@ -2,18 +2,23 @@ var api = require('ripple-gateway-data-sequelize-adapter');
 var send = require("../lib/send_payment");
 var nconf = require("../config/nconf");
 var build_payment = require('../lib/build_payment');
+var gateway = require('../lib/gateway');
 
 process.env.DATABASE_URL = nconf.get('DATABASE_URL');
 
 function workJob() {
 
-  api.rippleTransactions.readAll({ transaction_state: "queued" }, function(err, transactions) {
+  gateway.payments.listOutgoing(function(err, transactions) {
     if (!err) {
       var transaction = transactions[0];
       if (transaction) {
         api.rippleAddresses.read(transaction.to_address_id, function(err, address) {
-          console.log(address.address, transaction.to_currency + transaction.to_issuer)
-          build_payment(address.address, transaction.to_amount, transaction.to_currency, function(err, payment) {
+          var address = address.address,
+              amount = transaction.to_amount,
+              currency = transaction.to_currency;
+
+          build_payment(address, amount, currency, function(err, payment) {
+
             if (err) { 
               transaction.transaction_state = 'error';
               transaction.save().complete(function(){
@@ -22,7 +27,9 @@ function workJob() {
               return;
             }
             send(payment, function(err, payment){
+
               if (err) { setTimeout(workJob, 500); return }
+
               if (payment.success) {
                 transaction.transaction_state = 'sent';
                 transaction.save().complete(function(){
@@ -45,3 +52,6 @@ function workJob() {
 }
 
 workJob();
+
+console.log('Sending outgoing ripple payments from the queue.');
+
