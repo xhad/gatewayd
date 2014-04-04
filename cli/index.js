9ecfe3f36c4dcd358e478f6086c392a502fefdb0
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-03 21:47:37 -0700

    [FEATURE] Refactor cli registerUser function to cli module.

---
+++ cli/index.js
@@ -0,0 +1,7 @@
+
+var registerUser = require("./register_user");
+
+
+module.exports = {
+  registerUser: registerUser
+}
---
+++ cli/register_user.js
@@ -0,0 +1,40 @@
+var gateway = require("../lib/gateway.js");
+var PrettyPrintTable = require('../lib/pretty_print_tables.js');
+
+/**
+* Register a User
+* - creates external account named "default"
+* - creates ripple address as provided
+*
+* @param {string} name
+* @param {string} rippleAddress 
+* @param {string} password
+* @returns {User}, {ExternalAccount}, {RippleAddress}
+*/
+
+function registerUser(username, password, rippleAddress){
+
+  var opts = {
+    name: username,
+    password: password,
+    rippleAddress: rippleAddress
+  };
+
+  console.log(username, password, rippleAddress);
+  console.log(gateway.users.register);
+
+  gateway.users.register(opts, function(err, user) {
+    console.log(err, user);
+    console.log("### CREATED USER ###");
+    PrettyPrintTable.users([user]);
+    console.log("### CREATED RIPPLE ADDRESSES ###");
+    PrettyPrintTable.rippleAddresses([user.hosted_address, user.ripple_address]);
+    console.log("### CREATED EXTERNAL ACCOUNT ###");
+    PrettyPrintTable.externalAccounts([user.external_account]);
+    console.log();
+  });
+
+};
+
+module.exports = registerUser;
+
