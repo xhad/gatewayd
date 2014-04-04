08fff28df7783e0b99e06b47ffc2af0da7bc9aec
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-03 22:59:55 -0700

    [FEATURE] Refactor bin/gateway fund_hot_walelt.

--- cli/index.js
+++ cli/index.js
@@ -1,9 +1,12 @@
 
 var registerUser = require('./register_user');
 var listUsers = require('./list_users');
+var fundHotWallet = require('./fund_hot_wallet');
 
 
 module.exports = {
   registerUser: registerUser,
-  listUsers: listUsers
+  listUsers: listUsers,
+  fundHotWallet: fundHotWallet
 }
+
