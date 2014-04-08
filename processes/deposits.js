1a638f029deb1644b90d4ae2b9d4eb278d53962a
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-08 13:15:35 -0700

    [CHORE] Refactor away abstract calls.

--- processes/deposits.js
+++ processes/deposits.js
@@ -1,7 +1,6 @@
 var queue = require('../lib/deposit_queue.js');
 
 var config = require('../config/nconf.js');
-var abstract = require('../lib/abstract.js');
 var api = require("ripple-gateway-data-sequelize-adapter");
 var sql = require('../node_modules/ripple-gateway-data-sequelize-adapter/lib/sequelize.js');
 var gateway = require('../lib/gateway.js');
@@ -10,52 +9,57 @@ queue.on('deposit', function(deposit) {
 
   sql.transaction(function(t) {
 
-    abstract.getExternalAccountRippleAddress(deposit.external_account_id, function(err, address) {
-      if (err) {
+    api.externalAccounts.read({ id: externalAccountId }, function(err, account) {
+      if (err) { fn(err, null); return; };
 
-        tx.rollback();
-        return;
-      }
+      api.rippleAddresses.read({ user_id: account.user_id }, function(err, addresses){
+        if (err && addresses[0]) {
 
-      var opts = {
-        to_address_id: address.id,
-        amount: deposit.amount,
-        currency: deposit.currency,
-      };
-
-      gateway.payments.enqueueOutgoing(opts, function(err, payment) {
-        if (err) {
-
-          t.rollback();
+          tx.rollback();
           return;
         }
 
-        if (payment) {
-          var opts = {
-            id: deposit.id,
-            ripple_address_id: payment.id
-          };
+        var address = addresses[0];
 
-          gateway.deposits.finalize(opts, function(err, deposit) {
-            if (err) {
-
-              t.rollback();
-              return;
-            }
-
-            t.commit();
-            console.log(deposit.toJSON());
-
-          });
-        } else {
-          t.rollback();
-          return;
+        var opts = {
+          to_address_id: address.id,
+          amount: deposit.amount,
+          currency: deposit.currency,
         };
+
+        gateway.payments.enqueueOutgoing(opts, function(err, payment) {
+          if (err) {
+
+            t.rollback();
+            return;
+          }
+
+          if (payment) {
+            var opts = {
+              id: deposit.id,
+              ripple_address_id: payment.id
+            };
+
+            gateway.deposits.finalize(opts, function(err, deposit) {
+              if (err) {
+
+                t.rollback();
+                return;
+              }
+
+              t.commit();
+              console.log(deposit.toJSON());
+
+            });
+          } else {
+            t.rollback();
+            return;
+          };
+        });
       });
     });
   });
 });
-
 
 queue.work();
 
