ba57697a813cc64b3d057ecbe3bdf5dfc9994f4f
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-07 21:20:25 -0700

    [FEATURE] Implement bin/gateway list_outgoing_payments.

---
+++ cli/list_outgoing_payments.js
@@ -0,0 +1,16 @@
+var gateway = require("../lib/gateway.js");
+var PrettyPrintTable = require('../lib/pretty_print_tables.js');
+
+/**
+* List Outgoing Payments
+*
+* @returns [{Deposits}]
+*/
+
+function listOutgoingPayments() {
+  gateway.payments.listOutgoing(function(err, payments) {
+    PrettyPrintTable.payments(payments);
+  });
+}
+
+module.exports = listOutgoingPayments;
