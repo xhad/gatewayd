5326c6323f0ef3ad1d829095ddb76f42c171ea31
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-08 12:33:42 -0700

    [CHORE] Refactor call from abstract to data model.

--- http/controllers/users/index.js
+++ http/controllers/users/index.js
@@ -1,16 +1,14 @@
 var gateway = require('./../../../lib/gateway');
-var abstract = require('./../../../lib/abstract');
-var api = require('ripple-gateway-data-sequelize-adapter');
 
 function externalAccounts (req, res) {
-  api.externalAccounts.readAll({ user_id: req.params.id }, function(err, accounts) {
+  gateway.data.externalAccounts.readAll({ user_id: req.params.id }, function(err, accounts) {
     if (err) { res.send(500, { error: err }); return; }
     res.send({ externalAccounts: accounts });
   });
 };
 
 function externalTransactions(req, res) {
-  abstract.readAllUserExternalTransactions(req.params.id, function(err, transactions) {
+  gateway.data.externalTransactions.forUser(req.params.id, function(err, transactions) {
     if (err) {
       res.send(500, { error: err });
     } else {
@@ -21,7 +19,7 @@ function externalTransactions(req, res) {
 
 function rippleAddresses(req, res) {
 
-  api.rippleAddresses.readAll({ user_id: req.params.id }, function(err, addresses) {
+  gateway.data.rippleAddresses.readAll({ user_id: req.params.id }, function(err, addresses) {
     if (err) { res.send(500, { error: err }); return; }
     res.send({ rippleAddresses: addresses });
   });
