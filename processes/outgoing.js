142d27ee96298b494c6e67342e567374224ad7fb
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-07 20:50:59 -0700

    [FEATURE] Confirm payment after sending.

--- processes/outgoing.js
+++ processes/outgoing.js
@@ -3,10 +3,23 @@ var send = require("../lib/send_payment");
 var nconf = require("../config/nconf");
 var build_payment = require('../lib/build_payment');
 var gateway = require('../lib/gateway');
+var Client = require('ripple-rest-client');
 
 process.env.DATABASE_URL = nconf.get('DATABASE_URL');
 
-function workJob() {
+var client = new Client({
+    api: nconf.get('RIPPLE_REST_URL'),
+    account: nconf.get('gateway_hot_wallet').address,
+    secret: ''
+});
+
+function timeoutAndLoop() {
+  setTimeout(function(){ 
+    processOutgoingPayment(processOutgoingPayment);
+  }, 1000);
+}
+
+function processOutgoingPayment(callback) {
 
   gateway.payments.listOutgoing(function(err, transactions) {
     if (!err) {
@@ -17,47 +30,66 @@ function workJob() {
               amount = transaction.to_amount,
               currency = transaction.to_currency;
 
+          console.log(address);
+          console.log(amount);
+          console.log(currency);
           build_payment(address, amount, currency, function(err, payment) {
 
-            if (err) { 
+            if (err) {
               console.log(err);
-              if (err == 'No paths found') { 
+              if (err == 'No paths found') {
                 transaction.transaction_state = 'no_path_found';
-                transaction.save().complete(function(){
-                  setTimeout(workJob, 1000);
-                });
+                timeoutAndLoop();
                 return;
               } else {
-                setTimeout(workJob, 1000);
+                timeoutAndLoop();
                 return;
               }
             }
             send(payment, function(err, payment){
+                console.log('ERROR', err);
+              if (err) { 
+                timeoutAndLoop();
+                return;
 
-              if (err) { setTimeout(workJob, 1000); return }
+                if (payment.success) {
+                  transaction.transaction_state = 'sent';
+                  transaction.save().complete(function(){
+                    client.confimPayment(payment.client_resource_id, function(err, payment){
 
-              if (payment.success) {
-                transaction.transaction_state = 'sent';
-                transaction.save().complete(function(){
-                  setTimeout(workJob, 1000);
-                });
-              } else {
-                setTimeout(workJob, 1000);
+                          if(err){
+                            console.log('error:', err);
+                            timeoutAndLoop();
+                            return;
+                          } else {
+                            transaction.transaction_hash = payment.hash;
+                            transaction.save();
+                            timeoutAndLoop();
+                              return
+                          }
+
+                      });
+                  });
+                } else {
+                  timeoutAndLoop();
+                  return;
+                }
               }
             });
-          });          
+          
+          });
         });
       } else {
-        setTimeout(workJob, 1000);
+        timeoutAndLoop();
       }
     } else {
-      setTimeout(workJob, 1000);
+      timeoutAndLoop();
     }
   });
 
 }
 
-workJob();
+processOutgoingPayment(processOutgoingPayment);
 
 console.log('Sending outgoing ripple payments from the queue to Ripple REST.');
 
