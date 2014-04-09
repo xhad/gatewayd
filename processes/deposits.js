ce2b8f48685d551c82ec98c4bbd01cb8af450e01
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-09 09:11:53 -0700

    [FEATURE] Use deposit middleware framework with 2% fee.

--- processes/deposits.js
+++ processes/deposits.js
@@ -1,66 +1,17 @@
-var gateway = require('../');
+var DepositProcessor = require('../lib/deposit_processor.js');
 
-var queue = require('../lib/deposit_queue.js');
-var sql = require('../node_modules/ripple-gateway-data-sequelize-adapter/lib/sequelize.js');
+depositProcessor = new DepositProcessor(function(deposit, enqueueOutgoingPayment) {
 
-
-queue.on('deposit', function(deposit) {
-
-  sql.transaction(function(t) {
-
-    gateway.data.externalAccounts.read({ id: externalAccountId }, function(err, account) {
-      if (err) { fn(err, null); return; };
-
-      gateway.data.rippleAddresses.read({ user_id: account.user_id }, function(err, addresses){
-        if (err && addresses[0]) {
-
-          tx.rollback();
-          return;
-        }
-
-        var address = addresses[0];
-
-        var opts = {
-          to_address_id: address.id,
-          amount: deposit.amount,
-          currency: deposit.currency,
-        };
-
-        gateway.payments.enqueueOutgoing(opts, function(err, payment) {
-          if (err) {
-
-            t.rollback();
-            return;
-          }
-
-          if (payment) {
-            var opts = {
-              id: deposit.id,
-              ripple_address_id: payment.id
-            };
-
-            gateway.deposits.finalize(opts, function(err, deposit) {
-              if (err) {
-
-                t.rollback();
-                return;
-              }
-
-              t.commit();
-              console.log(deposit.toJSON());
-
-            });
-          } else {
-            t.rollback();
-            return;
-          };
-        });
-      });
-    });
+  // 2% Fee for Gateway Deposits, for instance
+  var amount = deposit.amount * 0.98;
+  
+  enqueueOutgoingPayment(null, {
+    to_address_id: deposit.to_address_id,
+    amount: amount,
+    currency: deposit.currency
   });
+  
 });
 
-queue.work();
-
-console.log('Processing deposits from the inbound asset deposit queue.');
+depositProcessor.start();
 
