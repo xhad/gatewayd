7687f9b5121b3b1c742981aa74bced7803aaa083
Steven Zeiler <zeiler.steven@gmail.com>
2014-05-16 14:03:59 -0700

    [CHORE] Refactor withdrawal processor to loop, use transaction.

--- processes/withdrawals.js
+++ processes/withdrawals.js
@@ -2,63 +2,91 @@ var gateway = require(__dirname+'/../');
 
 var queue = require(__dirname+'/../lib/queues/withdrawal_processor.js');
 var sql = require(__dirname+'/../node_modules/ripple-gateway-data-sequelize/lib/sequelize.js');
+var TIMEOUT = 1000;
+var FEE = 1 - gateway.config.get("WITHDRAWAL_FEE");
 
-queue.on('payment:withdrawal', function(payment) {
+function getIncomingTransaction(fn){
+  gateway.models.rippleTransactions.find({ where: { incoming: true }})
+  .complete(function(fn);
+}
 
-  console.log('received a new payment event');
-  console.log(payment.toJSON());
-  
-  gateway.data.rippleAddresses.read(payment.from_address_id, function(err, address) {
-    
-    if (err || !address) {
-      console.log('no address found');
-      return;
+function finalizeRippleTransaction(rippleTransaction, sqlTransaction, done){
+  var query = 'update ripple_transactions set "transaction_state" = "succeeded"';
+    query += 'where id = '+rippleTransaction.id+' and "transaction_state" = "incoming"';
+  sql.query(query).success(function(){
+    sqlTransaction.commit();
+    done();
+  }).error(function(){
+    sqlTransaction.rollback;
+    done();
+  });
+}
+
+function getTransactionRippleAddress(rippleTransaction, function(err, address){
+  var query = { where: { id: payment.from_address_id }};
+  gateway.data.rippleAddresses.find(query, function(err, address) {
+    if (err){
+      fn(err, null);
+    } else if (address){
+      fn(null, address);
+    } else {
+      fn('no address found', null);
     }
-
-    console.log(address);
-      
-    sql.transaction(function(t) {
-
-      gateway.data.externalTransactions.create({
-        deposit: false,
-        amount: payment.to_amount * (1 - gateway.config.get("WITHDRAWAL_FEE")),
-        currency: payment.to_currency,
-        status: 'queued',
-        ripple_transaction_id: payment.id,
-        external_account_id: address.tag
-      }, function(err, withdrawal) {
-
-        console.log(err, withdrawal);
-        
-        if (err) {
-          t.rollback();
-          return;
-        } 
-        
-        gateway.data.rippleTransactions.update({
-          id: payment.id,
-          transaction_state: 'tesSUCCESS'
-        }, function(err, rippleTransaction) {
-
-          if (err) {
-            t.rollback();
-            return;
-          } else {
-            if (rippleTransaction.transaction_state == 'incoming') {
-              t.rollback();
-              return;
-            } 
-            t.commit();
-            console.log('commit!');
-          }
-
-        })
-      });
-    });
   });
 });
 
-queue.work();
+function handleIncoming(rippleTransaction, done){
+  getTransactionRippleAddress(function(err, address)
+    if (err) { console.log('error', err); done(); return; };
+    sql.transaction(function(sqlTransaction) {
+      createWithdrawal(rippleTransaction, address, sqlTransaction, function(err){
+        if (err) { console.log('error', err); done(); return; };
+        finalizeRippleTransaction(rippleTransaction, sqlTransaction, done);
+      })
+    });
+  })
+});
+
+function createWithdrawal(rippleTransaction, address, sqlTransaction, callback){
+  gateway.data.externalTransactions.create({
+    deposit: false,
+    amount: rippleTransactoin.to_amount * (1 - gateway.config.get("WITHDRAWAL_FEE")),
+    currency: rippleTransaction.to_currency,
+    status: 'queued',
+    ripple_transaction_id: payment.id,
+    external_account_id: address.tag
+  }, function(err, withdrawal) {
+    if (err) {
+      sqlTransaction.rollback();
+      callback(err);
+    } else if (withdrawal) {
+      callback();
+    } else {
+      sqlTransaction.rollback();
+      callback('withdrawal not found');
+    }
+  });
+}
+
+function getNextIncomingTransaction(callback){
+  getIncomingTransaction(function(err, transaction){
+    if (err) {
+      setTimeout(function(){
+        callback(callback);
+      }, TIMEOUT);
+    } else if {
+      handleIncomingTransaction(transaction, function(){
+        callback(callback);
+      });
+    } else {
+      setTimeout(function(){
+        callback(callback);
+      }, TIMEOUT);
+    }
+  });
+}
+
+getNextIncomingTransaction(getNextIncomingTransaction);
 
 console.log('Processing withdrawals from the inbound ripple payment queue.');
 
