5e1c21292ca462db02e1bd28155d619aa2aa433b
Steven Zeiler <zeiler.steven@gmail.com>
2014-05-19 09:21:13 -0700

    [FEATURE] Make listener instantiatable, reusable.

--- processes/incoming.js
+++ processes/incoming.js
@@ -2,11 +2,16 @@ var gateway = require(__dirname+'/../');
 
 var Listener = require(__dirname+'/../lib/ripple/listener.js');
 
-var listener = new Listener();
+var listener = new Listener({
+  address: gateway.config.get('COLD_WALLET'),
+  startHash: gateway.config.get('LAST_PAYMENT_HASH')
+});
 
-listener.onPayment = function(payment) {
 
-  if (payment && payment.destination_account == gateway.config.get('COLD_WALLET')) {
+listener.vent.on("payment", function(payment) {
+  console.log("PAYMENT", payment);
+
+  if (payment.destination_account == gateway.config.get('COLD_WALLET')) {
     var dt = payment.destination_tag;
     var state = payment.result;
     var hash = payment.hash;
@@ -22,23 +27,19 @@ listener.onPayment = function(payment) {
         gateway.payments.recordIncoming(dt, currency, amount, 'incoming', hash, function(err, record) {
           if (err) {
             console.log('error:', err); 
-
-          } else {
             try {
               console.log(record.toJSON()); 
-
             } catch(e) {
               console.log('error', e);
-
             }
           }
         });
       }
     }
   };
-};
+});
 
-listener.start(gateway.config.get('LAST_PAYMENT_HASH'));
+listener.start();
 
 console.log('Listening for incoming ripple payments from Ripple REST.');
 
