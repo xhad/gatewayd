aa6f55133ba9782cac2f7335925a895398018b96
Abiy Seifu <abiy@ripple.com>
2014-05-27 23:56:06 -0700

    [TEST] write basic status code tests for HTTP controllers

---
+++ test/http/get_liabilities_balance.js
@@ -0,0 +1,29 @@
+var request = require('supertest');
+var app = require(__dirname+'/../../lib/app.js');
+var gateway = require(__dirname+'/../../');
+
+describe('get cold wallet balance', function(){
+
+  it('should return unauthorized without credentials', function(done){
+    request(app)
+      .get('/v1/liabilities')
+      .expect(401)
+      .end(function(err, res){
+        if (err) throw err;
+        done();
+      });
+  });
+
+  it('should return successfully with credentials', function(done){
+    request(app)
+      .get('/v1/liabilities')
+      .auth('admin@'+gateway.config.get('DOMAIN'), gateway.config.get('KEY'))
+      .expect(200)
+      .end(function(err, res){
+        if (err) throw err;
+        done();
+      });
+  });
+
+});
+
