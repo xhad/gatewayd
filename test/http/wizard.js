0833faeadd3c94c323a71e6ef96af22969141ae7
Abiy Seifu <abiy@ripple.com>
2014-06-03 00:11:29 -0700

    [FEATURE] validate ripple address, porstgres url, ripple-rest url
    [CHORE] breakup public http controllers.
    
    [TEST] validate trust amount

--- test/http/wizard.js
+++ test/http/wizard.js
@@ -3,7 +3,18 @@ var app = require(__dirname+'/../../lib/app.js');
 var gateway = require(__dirname+'/../../');
 var assert = require('assert');
 
+
 describe('set configuration profile', function(){
+
+  function findError(errors, field) {
+    var errorFound;
+    errors.forEach(function(error){
+      if (error.field === field) {
+        errorFound = error;
+      }
+    });
+    return errorFound;
+  }
 
   it('should return successfully with valid configuration', function(done){
     request(app)
@@ -11,7 +22,7 @@ describe('set configuration profile', function(){
       .send( {
           config: {
             postgres_url: 'postgres://postgres:password@localhost:5432/ripple_gateway',
-            ripple_rest_url: 'http://localhost:5990',
+            ripple_rest_url: 'http://localhost:5990/v1',
             ripple_address: 'rMinhWxZz4jeHoJGyddtmwg6dWhyqQKtJz',
             currencies: {
               SWD: 0
@@ -26,13 +37,35 @@ describe('set configuration profile', function(){
       });
   });
 
+  it('should return error with no ripple_rest_url', function(done){
+    request(app)
+      .post('/v1/config/wizard')
+      .send( {
+        config: {
+          ripple_rest_url: 'https://localhost:5990/v1',
+          ripple_address: 'somerippleaddress',
+          currencies: {
+            SWD: 110
+          }
+        }
+      }
+    )
+      .expect(400)
+      .end(function(err, res){
+        assert(findError(res.body.errors, 'postgres_url'));
+        assert(findError(res.body.errors, 'postgres_url').message == 'please provide postgres_url');
+        if (err) throw err;
+        done();
+      });
+  });
+
   it('should return error with no currencies', function(done){
     request(app)
       .post('/v1/config/wizard')
       .send( {
         config: {
           postgres_url: 'postgres://postgres:password@localhost:5432/ripple_gateway',
-          ripple_rest_url: 'http://localhost:5990',
+          ripple_rest_url: 'http://localhost:5990/v1',
           ripple_address: 'rMinhWxZz4jeHoJGyddtmwg6dWhyqQKtJz'
         }
       }
@@ -46,15 +79,6 @@ describe('set configuration profile', function(){
      });
   });
 
-  function findError(errors, field) {
-    var errorFound;
-    errors.forEach(function(error){
-      if (error.field === field) {
-        errorFound = error;
-      }
-    });
-    return errorFound;
-  }
 
   it('should return error with no ripple_address', function(done){
     request(app)
@@ -62,7 +86,7 @@ describe('set configuration profile', function(){
       .send( {
         config: {
           postgres_url: 'postgres://postgres:password@localhost:5432/ripple_gateway',
-          ripple_rest_url: 'http://localhost:5990'
+          ripple_rest_url: 'http://localhost:5990/v1'
         }
       }
     )
@@ -77,15 +101,15 @@ describe('set configuration profile', function(){
     });
   });
 
-  it('should return error with no ripple_rest_url', function(done){
+  it('should return error with no postgres_url', function(done){
     request(app)
       .post('/v1/config/wizard')
       .send( {
         config: {
           postgres_url: 'postgres://postgres:password@localhost:5432/ripple_gateway',
-          ripple_address: 'somerippleaddress',
+          ripple_address: 'rMinhWxZz4jeHoJGyddtmwg6dWhyqQKtJz',
           currencies: {
-            XAG: 110
+            SWD: 110
           }
         }
       }
@@ -99,23 +123,95 @@ describe('set configuration profile', function(){
     });
   });
 
-  it('should return error with no postgres_url', function(done){
+
+
+  it('should return error with an invalid postgres_url', function(done){
     request(app)
       .post('/v1/config/wizard')
       .send( {
         config: {
-          ripple_rest_url: 'https://localhost:5990/v1',
-          ripple_address: 'somerippleaddress',
+          postgres_url: 'invalidPostgresUrl',
+          ripple_rest_url: 'http://localhost:5990/v1',
+          ripple_address: 'rMinhWxZz4jeHoJGyddtmwg6dWhyqQKtJz',
           currencies: {
-            XAG: 110
+            SWD: 0
           }
         }
       }
     )
     .expect(400)
     .end(function(err, res){
-      assert(findError(res.body.errors, 'postgres_url'));
-      assert(findError(res.body.errors, 'postgres_url').message == 'please provide postgres_url');
+      assert(findError(res.body.errors, 'invalid_postgres_url'));
+      assert(findError(res.body.errors, 'invalid_postgres_url').message == 'please provide a valid postgres_url');
+      if (err) throw err;
+      done();
+    });
+  });
+
+  it('should return error with an invalid ripple_rest_url', function(done){
+    request(app)
+      .post('/v1/config/wizard')
+      .send( {
+        config: {
+          postgres_url: 'postgres://postgres:password@localhost:5432/ripple_gateway',
+          ripple_rest_url: 'inValidRippleRestUrl',
+          ripple_address: 'rMinhWxZz4jeHoJGyddtmwg6dWhyqQKtJz',
+          currencies: {
+            SWD: 0
+          }
+        }
+      }
+    )
+    .expect(400)
+    .end(function(err, res){
+      assert(findError(res.body.errors, 'invalid_ripple_rest_url'));
+      assert(findError(res.body.errors, 'invalid_ripple_rest_url').message == 'please provide a valid ripple_rest_url');
+      if (err) throw err;
+      done();
+    });
+  });
+
+  it('should return error with an invalid ripple_address', function(done){
+    request(app)
+      .post('/v1/config/wizard')
+      .send( {
+        config: {
+          postgres_url: 'postgres://postgres:password@localhost:5432/ripple_gateway',
+          ripple_rest_url: 'http://localhost:5990/v1',
+          ripple_address: 'invalidRippleAdddress',
+          currencies: {
+            SWD: 0
+          }
+        }
+      }
+    )
+    .expect(400)
+    .end(function(err, res){
+      assert(findError(res.body.errors, 'invalid_ripple_address'));
+      assert(findError(res.body.errors, 'invalid_ripple_address').message == 'please provide a valid ripple_address');
+      if (err) throw err;
+      done();
+    });
+  });
+
+  it('should return error with an invalid currency trust amount', function(done){
+    request(app)
+      .post('/v1/config/wizard')
+      .send( {
+        config: {
+          postgres_url: 'postgres://postgres:password@localhost:5432/ripple_gateway',
+          ripple_rest_url: 'http://localhost:5990/v1',
+          ripple_address: 'rMinhWxZz4jeHoJGyddtmwg6dWhyqQKtJz',
+          currencies: {
+            SWD: 'NaN'
+          }
+        }
+      }
+    )
+    .expect(400)
+    .end(function(err, res){
+      assert(findError(res.body.errors, 'invalid_currency_trust'));
+      assert(findError(res.body.errors, 'invalid_currency_trust').message == 'please provide a valid currency trust amount');
       if (err) throw err;
       done();
     });
