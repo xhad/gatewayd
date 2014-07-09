d8c123f6c184a9cdc319078336624ddac0ec814d
Abiy Seifu <abiy@ripple.com>
2014-07-09 15:31:47 -0700

    [TEST] fund hot wallet

---
+++ test/api/fund_hot_wallet.js
@@ -0,0 +1,33 @@
+var gateway = require(__dirname+'/../../');
+var assert = require('assert');
+
+var SECRET = process.env.RIPPLE_ACCOUNT_SECRET;
+
+describe('Fund hot wallet', function() {
+  if(SECRET) {
+    it('should successfully fund account should return trust lines', function(done){
+      this.timeout(5000);
+
+      gateway.api.fundHotWallet(1, 'XRP', SECRET, function(error, response){
+        assert.strictEqual(gateway.config.get('HOT_WALLET').address, response.destination_account);
+        assert.strictEqual(gateway.config.get('COLD_WALLET'), response.source_account);
+        assert.strictEqual(response.result, 'tesSUCCESS');
+        done();
+      });
+    });
+
+
+    it('should fail due to invalid secret', function(done){
+      this.timeout(5000);
+
+      gateway.api.fundHotWallet(1, 'XRP', 'invalidSecret', function(error, response){
+        assert.strictEqual(error, 'Please ensure the address and secret correspond to a valid account that has a positive XRP balance.');
+        done();
+      });
+
+    });
+  } else {
+    it.skip('should successfully fund account');
+  }
+
+});
