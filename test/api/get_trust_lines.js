d4219dbd92af30b605995a81d61deba356de74d7
Abiy Seifu <abiy@ripple.com>
2014-07-09 15:31:58 -0700

    [TEST] get trust lines

--- test/api/get_trust_lines.js
+++ test/api/get_trust_lines.js
@@ -2,11 +2,30 @@ var gateway = require(__dirname+'/../../');
 var assert = require('assert');
 
 describe('Get trust lines', function() {
-  it('\nbetween hot wallet and cold wallet', function(done){
+  it('should get trust lines between hot wallet and cold wallet', function(done){
+    var options = {
+      hotWallet: gateway.config.get('HOT_WALLET').address,
+      coldWallet: gateway.config.get('COLD_WALLET')
+    };
 
     gateway.api.getTrustLines(function(error, response){
-      console.log('err::',error);
-      console.log('res::',response);
+      assert.strictEqual(response[0].account, options.hotWallet);
+      assert.strictEqual(response[0].counterparty, options.coldWallet);
+      done();
+    });
+  });
+
+  //set hot wallet to an invalid ripple address
+
+  it('should fail because of invalid hot wallet address', function(done){
+    var options = {
+      hotWallet: 'r1234...e',
+      coldWallet: gateway.config.get('COLD_WALLET')
+    };
+
+    gateway.api.getTrustLines(function(error, response){
+      assert.notStrictEqual(response[0].account, options.hotWallet);
+      assert.strictEqual(response[0].counterparty, options.coldWallet);
       done();
     });
   });
