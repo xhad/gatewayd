dd8bc794eb308a37a3f3c8afd444ad0336248007
Abiy Seifu <abiy@ripple.com>
2014-07-15 15:52:07 -0700

    [TEST] pass options argument

--- test/api/refund_cold_wallet.js
+++ test/api/refund_cold_wallet.js
@@ -5,7 +5,12 @@ describe('Refund cold wallet', function(){
 
   it('should successfully issue currency', function(done){
     this.timeout(5000);
-    gateway.api.refundColdWallet('SWD', 1, function(error, response){
+    var options = {
+      amount: 1,
+      currency: 'SWD'
+    };
+
+    gateway.api.refundColdWallet(options, function(error, response){
       assert.strictEqual(response.destination_account, gateway.config.get('COLD_WALLET'));
       assert.strictEqual(response.source_account, gateway.config.get('HOT_WALLET').address);
       assert.strictEqual(response.destination_amount.currency, 'SWD');
@@ -15,7 +20,11 @@ describe('Refund cold wallet', function(){
 
   it('should fail to refund cold wallet due to insufficient funds of issued currency', function(done){
     this.timeout(5000);
-    gateway.api.refundColdWallet('PDG', 1, function(error, response){
+    var options = {
+      amount: 1,
+      currency: 'PGP'
+    };
+    gateway.api.refundColdWallet(options, function(error, response){
       assert.strictEqual(error, 'Path could not send partial amount. Please ensure that the source_address has sufficient funds (in the source_amount currency, if specified) to execute this transaction.');
       assert.strictEqual(typeof response, 'undefined');
       done();
