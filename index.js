ea932314e11899736abec0070ec6c2918d7195e1
Steven Zeiler <zeiler.steven@gmail.com>
2014-08-02 22:34:21 -0700

    [FEATURE] Add gatewayd.server to global namespace.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ exports.api = requireAll(__dirname+'/lib/api/');
 exports.errors = requireAll(__dirname+'/lib/errors/');
 exports.ripple = require(__dirname +'/lib/ripple/');
 exports.validator = require(__dirname+'/lib/validator.js');
+exports.server = require(__dirname+'/lib/app.js');
 
 require(__dirname+'/Gatewaydfile')(exports);
 
