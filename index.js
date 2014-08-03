adc0037aee341a4fc2cddda23662a817f84c229c
Steven Zeiler <zeiler.steven@gmail.com>
2014-08-02 22:32:41 -0700

    [FEATURE] Gatewaydfile for custom plugins..

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-var config = require(__dirname + '/config/config.js');
-var data   = require(__dirname +'/lib/data/');
-var ripple = require(__dirname +'/lib/ripple/');
-var validator = require(__dirname+'/lib/validator.js');
 var requireAll = require('require-all-to-camel');
 
-/** @module Api */
-var api = requireAll(__dirname+'/lib/api/');
-var errors = requireAll(__dirname+'/lib/errors/');
+exports.config = require(__dirname + '/config/config.js');
+exports.data   = require(__dirname +'/lib/data/');
+exports.api = requireAll(__dirname+'/lib/api/');
+exports.errors = requireAll(__dirname+'/lib/errors/');
+exports.ripple = require(__dirname +'/lib/ripple/');
+exports.validator = require(__dirname+'/lib/validator.js');
 
-module.exports = {
-  config: config,
-  data: data,
-  api: api,
-  errors: errors,
-  validator: validator,
-  ripple: ripple
-}; 
+require(__dirname+'/Gatewaydfile')(exports);
 
