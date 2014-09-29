a50dfd067167b1c76c9b30d2b4d15303d2756f14
Steven Zeiler <zeiler.steven@gmail.com>
2014-09-29 16:49:52 -0700

    [FEATURE] Add gatewayd-setup-wizard-plugin to dependencies.
    [TASK] Add commented out wizard plugin to Gatewaydfile.
    
    [FIX] Bump wizard patch version.

--- Gatewaydfile.js
+++ Gatewaydfile.js
@@ -1,5 +1,10 @@
 
 module.exports = function(gatewayd) {
-
+  //  ADD PLUGINS HERE, SUCH AS SETUP WIZARD
+  //  const WizardPlugin = require('gatewayd-setup-wizard-plugin');
+  //  var wizardPlugin = new WizardPlugin({
+  //    gatewayd: gatewayd
+  //  });
+  //  gatewayd.server.use('/', wizardPlugin.router);
 }
 
