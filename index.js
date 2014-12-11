851dbc78a61bc00f6e1cc5488645cab74cf6afdc
Steven Zeiler <zeiler.steven@gmail.com>
2014-12-10 16:44:01 -0800

    [FEATURE] gatewayd.features for beta feature flags.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ exports.validator = require(__dirname+'/lib/validator.js');
 exports.server = require(__dirname+'/lib/app.js');
 exports.processes = require(__dirname+'/lib/processes/');
 exports.logger = require(__dirname+'/lib/data/logs.js');
+exports.features = require(__dirname+'/lib/features');
 
 var initializers = requireAll(__dirname+'/config/initializers/');
 
