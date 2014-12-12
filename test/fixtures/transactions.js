e2383c73214aad02033904c07a348be630b77c42
Haroun Serang <haroun@ripple.com>
2014-12-12 15:46:51 -0800

    [FEATURE] Create New Fields and Update Existing
    [TEST] Update Tests for New Schema
    
    currency and amount are no longer required and will be deprecated
    source account_id, amount and currency are new
    destination account_id, amount and currency are new
    memos is new

--- test/fixtures/transactions.js
+++ test/fixtures/transactions.js
@@ -15,5 +15,25 @@ module.exports = {
     partial_payment: false,
     no_direct_ripple: false,
     destination_tag: '0'
+  },
+
+  externalTransaction: {
+    external_account_id: 1,
+    source_amount: 4.32,
+    source_currency: 'JPY',
+    source_account_id: 432,
+    destination_amount: 5.22,
+    destination_currency: 'GBP',
+    destination_account_id: 234,
+    amount: 5,
+    currency: 'USD',
+    invoice_id: 'bc7e8a24e2911a5827c9b33d618531ef094937f2b3803a591c625d0ede1fffc6',
+    memos: 'this is a memo 123',
+    deposit: true
+  },
+
+  minimalExternalTransaction: {
+    external_account_id: 2,
+    deposit: true
   }
 };
