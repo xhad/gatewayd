efe2ba28bd1494d4af19d759cdd0421826764a05
Steven Zeiler <zeiler.steven@gmail.com>
2014-12-17 13:09:59 -0800

    [FEATURE] Supervisor to run all coroutines
    [FOlD] Pass jshint semicolon requirement
    
    [FOLD] Spell out variables
    
    [FOLD] Use gatewayd.logger instead of console.log
    
    [FOLD] Move timeout to constant at top of file

--- processes/incoming.js
+++ processes/incoming.js
@@ -1,55 +1,65 @@
-var Promise = require('bluebird');
-Error.stackTraceLimit = Infinity;
-
-process.on('uncaughtException', function(err) {
-  gatewayd.logger.error('Caught exception: ' + err);
-});
-
-Promise.onPossiblyUnhandledRejection(function(err) {
-  gatewayd.logger.error('Caught exception: ' + err);
-});
-
+var Promise                 = require('bluebird');
 var gatewayd                = require(__dirname+'/../');
-const RippleAccountMonitor  = require('ripple-account-monitor');
-const IncomingPayment       = require(__dirname+'/../lib/core/incoming_payment.js');
+var RippleAccountMonitor    = require('ripple-account-monitor');
+var IncomingPayment         = require(__dirname+'/../lib/core/incoming_payment.js');
 const coldWallet            = gatewayd.config.get('COLD_WALLET');
 const rippleRestBaseUrl     = gatewayd.config.get('RIPPLE_REST_API');
 
-const monitor = new RippleAccountMonitor({
-  rippleRestUrl: rippleRestBaseUrl,
-  account: coldWallet,
-  onTransaction: function(transaction, next) {
-    gatewayd.api.setLastPaymentHash(transaction.hash)
-      .then(function(hash){
-        gatewayd.logger.info('payment:hash set to:', hash);
-        next();
-      })
-      .error(function(error) {
-        gatewayd.logger.error('payment:set last payment hash:error', error);
-        next();
-      });
-  },
-  onPayment: function(paymentNotification, next) {
-    var incomingPayment = new IncomingPayment(paymentNotification);
-    incomingPayment.processPayment()
-      .then(function(processedPayment){
-        gatewayd.logger.info('payment:incoming:recorded', JSON.stringify(processedPayment));
-        next();
-      })
-      .error(function(error){
-        console.log(error);
-        gatewayd.logger.error('payment:incoming:error', error);
-        next();
-      });
-  },
-  onError: function(error) {
-    gatewayd.logger.error('RippleAccountMonitor::Error', error);
-  }
-});
-
-gatewayd.api.getOrFetchLastPaymentHash()
-  .then(function(paymentHash){
-    monitor.lastHash = paymentHash;
-    monitor.start();
-    logger.info('Listening for incoming ripple payments from Ripple REST, starting at', monitor.lastHash);
+function Monitor(gatewayd) {
+  return new RippleAccountMonitor({
+    rippleRestUrl: rippleRestBaseUrl,
+    account: coldWallet,
+    onTransaction: function(transaction, next) {
+      gatewayd.api.setLastPaymentHash(transaction.hash)
+        .then(function(hash){
+          gatewayd.logger.info('payment:hash set to:', hash);
+          next();
+        })
+        .error(function(error) {
+          gatewayd.logger.error('payment:set last payment hash:error', error);
+          next();
+        });
+    },
+    onPayment: function(paymentNotification, next) {
+      var incomingPayment = new IncomingPayment(paymentNotification);
+      incomingPayment.processPayment()
+        .then(function(processedPayment){
+          gatewayd.logger.info('payment:incoming:recorded', JSON.stringify(processedPayment));
+          next();
+        })
+        .error(function(error){
+          gatewayd.logger.error('payment:incoming:error', error);
+          next();
+        });
+    },
+    onError: function(error) {
+      gatewayd.logger.error('RippleAccountMonitor::Error', error);
+    }
   });
+}
+
+function start(gatewayd) {
+  var monitor = new Monitor(gatewayd);
+  gatewayd.api.getOrFetchLastPaymentHash()
+    .then(function(paymentHash){
+      monitor.lastHash = paymentHash;
+      monitor.start();
+      gatewayd.logger.info('Listening for incoming ripple payments from Ripple REST, starting at', monitor.lastHash);
+    });
+}
+
+if (gatewayd.features.isEnabled('supervisor')) {
+  module.exports = function(gatewayd) {
+    start(gatewayd);
+  };
+} else {
+  Error.stackTraceLimit = Infinity;
+  process.on('uncaughtException', function(err) {
+    gatewayd.logger.error('Caught exception: ' + err);
+  });
+  Promise.onPossiblyUnhandledRejection(function(err) {
+    gatewayd.logger.error('Caught exception: ' + err);
+  });
+  start(gatewayd);
+}
+
