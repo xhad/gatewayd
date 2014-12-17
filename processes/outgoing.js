0fa78dd8eb0c20f0cf056b1dcaf1ebb2bbfbcc20
Steven Zeiler <zeiler.steven@gmail.com>
2014-12-17 13:09:59 -0800

    [FEATURE] Supervisor to run all coroutines
    [FOlD] Pass jshint semicolon requirement
    
    [FOLD] Spell out variables
    
    [FOLD] Use gatewayd.logger instead of console.log
    
    [FOLD] Move timeout to constant at top of file

--- processes/outgoing.js
+++ processes/outgoing.js
@@ -2,24 +2,29 @@ var SqlMqWorker = require('sql-mq-worker');
 var OutgoingPaymentProcessor = require(__dirname+'/../lib/core/outgoing_payment_processor.js');
 var gatewayd = require(__dirname+'/../');
 
-var worker = new SqlMqWorker({
-  Class: gatewayd.data.models.rippleTransactions,
-  predicate: { where: {
-    state: 'outgoing'
-  }},
-  job: function(outgoingPayment, callback) {
-    var outgoingPaymentProcessor = new OutgoingPaymentProcessor(outgoingPayment);
-    outgoingPaymentProcessor.processOutgoingPayment(callback);
-  }
-});
+function worker(gatewayd) {
+  return new SqlMqWorker({
+    Class: gatewayd.data.models.rippleTransactions,
+    predicate: { where: {
+      state: 'outgoing'
+    }},
+    job: function(outgoingPayment, callback) {
+      var outgoingPaymentProcessor = new OutgoingPaymentProcessor(outgoingPayment);
+      outgoingPaymentProcessor.processOutgoingPayment(callback);
+    }
+  });
+}
 
-worker.start();
-
-process.on('uncaughtException', function(error) {
-  gatewayd.logger.error('exception', error);
-  gatewayd.logger.error('exception:stack', error.stack);
-  process.exit();
-});
-
-
+if (gatewayd.features.isEnabled('supervisor')) {
+  module.exports = function(gatewayd) {
+    worker(gatewayd).start();
+  };
+} else {
+  process.on('uncaughtException', function(error) {
+    gatewayd.logger.error('exception', error);
+    gatewayd.logger.error('exception:stack', error.stack);
+    process.exit();
+  });
+  worker(gatewayd).start();
+}
 
