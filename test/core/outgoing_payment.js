d54113176e97774ad145c6b1c919623bbd6bdac0
Steven Zeiler <zeiler.steven@gmail.com>
2015-01-05 17:42:28 -0800

    [TASK] Add new tests to package.json
    [FOLD] Add error handler in test

--- test/core/outgoing_payment.js
+++ test/core/outgoing_payment.js
@@ -283,6 +283,9 @@ describe('Sending a queued payment to ripple', function() {
         chai.assert.strictEqual(rippleTransaction.from_amount, 1.45);
         done();
       })
+      .error(function(error) {
+        done(new Error(error);
+      });
   });
 
 });
